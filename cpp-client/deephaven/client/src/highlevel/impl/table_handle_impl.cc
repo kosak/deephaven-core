fd54924aa7181ce4f8c613d3aa89f46f56ba3197
Corey Kosak <coreykosak@deephaven.io>
2022-05-16 15:27:00 -0400

    Justice League

--- cpp-client/deephaven/client/src/highlevel/impl/table_handle_impl.cc
+++ cpp-client/deephaven/client/src/highlevel/impl/table_handle_impl.cc
@@ -347,111 +347,12 @@ std::shared_ptr<TableHandleImpl> TableHandleImpl::asOfJoin(AsOfJoinTablesRequest
 }
 
 namespace {
-class SubscribeNubbin final : public Callback<> {
-  typedef deephaven::client::lowlevel::Server Server;
-
-public:
-  SubscribeNubbin(std::shared_ptr<Server> server, std::vector<int8_t> ticketBytes,
-      std::shared_ptr<internal::ColumnDefinitions> colDefs,
-      std::promise<void> promise, std::shared_ptr<TickingCallback> callback);
-  void invoke() final;
-
-  static std::shared_ptr<ThreadNubbin> sadClown_;
-
-private:
-  void invokeHelper();
-
-  std::shared_ptr<Server> server_;
-  std::vector<int8_t> ticketBytes_;
-  std::shared_ptr<internal::ColumnDefinitions> colDefs_;
-  std::promise<void> promise_;
-  std::shared_ptr<TickingCallback> callback_;
-};
-
-// A simple extension to arrow::Buffer that owns its DetachedBuffer storage
-class ZamboniBuffer : public arrow::Buffer {
-public:
-  explicit ZamboniBuffer(flatbuffers::DetachedBuffer buffer);
-
-private:
-  flatbuffers::DetachedBuffer buffer_;
-};
 std::shared_ptr<MutableColumnSource> makeColumnSource(const arrow::DataType &dataType);
 
-}  // namespace
 
-namespace {
-SubscribeNubbin::SubscribeNubbin(std::shared_ptr<Server> server, std::vector<int8_t> ticketBytes,
-    std::shared_ptr<internal::ColumnDefinitions> colDefs, std::promise<void> promise,
-    std::shared_ptr<TickingCallback> callback) :
-    server_(std::move(server)), ticketBytes_(std::move(ticketBytes)), colDefs_(std::move(colDefs)),
-        promise_(std::move(promise)), callback_(std::move(callback)) {}
-
-ZamboniBuffer::ZamboniBuffer(flatbuffers::DetachedBuffer buffer) :
-    arrow::Buffer(buffer.data(), int64_t(buffer.size())), buffer_(std::move(buffer)) {}
-
-void SubscribeNubbin::invoke() {
-  try {
-    invokeHelper();
-    // If you made it this far, then you have been successful!
-    promise_.set_value();
-  } catch (const std::exception &e) {
-    promise_.set_exception(std::make_exception_ptr(e));
-  }
-}
 
 constexpr const uint32_t deephavenMagicNumber = 0x6E687064U;
 
-void SubscribeNubbin::invokeHelper() {
-  arrow::flight::FlightCallOptions fco;
-  fco.headers.push_back(server_->makeBlessing());
-  auto *client = server_->flightClient();
-
-  arrow::flight::FlightDescriptor dummy;
-  char magicData[4];
-  uint32_t src = deephavenMagicNumber;
-  memcpy(magicData, &src, sizeof(magicData));
-
-  dummy.type = arrow::flight::FlightDescriptor::DescriptorType::CMD;
-  dummy.cmd = std::string(magicData, 4);
-  std::unique_ptr<arrow::flight::FlightStreamWriter> fsw;
-  std::unique_ptr<arrow::flight::FlightStreamReader> fsr;
-  okOrThrow(DEEPHAVEN_EXPR_MSG(client->DoExchange(fco, dummy, &fsw, &fsr)));
-
-  // Make a BarrageMessageWrapper
-  // ...Whose payload is a BarrageSubscriptionRequest
-  // ......which has BarrageSubscriptionOptions
-
-  flatbuffers::FlatBufferBuilder payloadBuilder(4096);
-
-  auto subOptions = CreateBarrageSubscriptionOptions(payloadBuilder,
-      ColumnConversionMode::ColumnConversionMode_Stringify, true, 0, 4096);
-
-  auto ticket = payloadBuilder.CreateVector(ticketBytes_);
-  auto subreq = CreateBarrageSubscriptionRequest(payloadBuilder, ticket, {}, {}, subOptions);
-  payloadBuilder.Finish(subreq);
-  // TODO(kosak): fix sad cast
-  const auto *payloadp = (int8_t*)payloadBuilder.GetBufferPointer();
-  const auto payloadSize = payloadBuilder.GetSize();
-
-  // TODO: I'd really like to just point this buffer backwards to the thing I just created, rather
-  // then copying it. But, eh, version 2.
-  flatbuffers::FlatBufferBuilder wrapperBuilder(4096);
-  auto payload = wrapperBuilder.CreateVector(payloadp, payloadSize);
-  auto messageWrapper = CreateBarrageMessageWrapper(wrapperBuilder, deephavenMagicNumber,
-      BarrageMessageType::BarrageMessageType_BarrageSubscriptionRequest, payload);
-  wrapperBuilder.Finish(messageWrapper);
-  auto wrapperBuffer = wrapperBuilder.Release();
-
-  auto buffer = std::make_shared<ZamboniBuffer>(std::move(wrapperBuffer));
-  okOrThrow(DEEPHAVEN_EXPR_MSG(fsw->WriteMetadata(std::move(buffer))));
-
-  auto threadNubbin = std::make_shared<ThreadNubbin>(std::move(fsr),
-      std::move(colDefs_), std::move(callback_));
-  sadClown_ = threadNubbin;
-  std::thread t(&ThreadNubbin::runForever, std::move(threadNubbin));
-  t.detach();
-}
 
 ThreadNubbin::ThreadNubbin(std::unique_ptr<arrow::flight::FlightStreamReader> fsr,
     std::shared_ptr<internal::ColumnDefinitions> colDefs, std::shared_ptr<TickingCallback> callback) :
