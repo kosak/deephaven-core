0362703427a68918faaab951c4d2c82766c2272f
Corey Kosak <coreykosak@deephaven.io>
2022-05-22 17:08:10 -0400

    uh oh

--- cpp-client/deephaven/client/include/private/deephaven/client/subscription/space_mapper.h
+++ cpp-client/deephaven/client/include/private/deephaven/client/subscription/space_mapper.h
@@ -5,6 +5,6 @@ class SpaceMapper {
 public:
   size_t addRange(size_t beginKey, size_t endKey);
   size_t eraseRange(size_t beginKey, size_t endKey);
-
+  void applyShift(size_t beginKey, size_t endKey, size_t destKey);
 };
 }  // namespace deephaven::client::subscription
--- cpp-client/deephaven/client/src/subscription/subscribed_table_state.cc
+++ cpp-client/deephaven/client/src/subscription/subscribed_table_state.cc
@@ -25,50 +25,9 @@ using deephaven::client::utility::stringf;
 
 namespace deephaven::client::subscription {
 namespace {
-void mapShifter(int64_t start, int64_t endInclusive, int64_t dest, std::map<int64_t, int64_t> *zm);
-void applyShiftData(const RowSequence &startIndex, const RowSequence &endInclusiveIndex,
-    const RowSequence &destIndex,
+// void mapShifter(int64_t start, int64_t endInclusive, int64_t dest, std::map<int64_t, int64_t> *zm);
+void applyShiftData(const RowSequence &firstIndex, const RowSequence &lastIndex, const RowSequence &destIndex,
     const std::function<void(int64_t, int64_t, int64_t)> &processShift);
-
-class MyRowSequence final : public RowSequence {
-  typedef std::map<int64_t, int64_t> data_t;
-public:
-  MyRowSequence(std::shared_ptr<data_t> data, data_t::const_iterator begin,
-      data_t::const_iterator end, size_t size);
-  ~MyRowSequence() final = default;
-  std::shared_ptr<RowSequenceIterator> getRowSequenceIterator() const final;
-  std::shared_ptr<RowSequenceIterator> getRowSequenceReverseIterator() const final;
-
-  void forEachChunk(const std::function<void(int64_t firstKey, int64_t lastKey)> &f) const final;
-
-  size_t size() const final {
-    return size_;
-  }
-
-private:
-  std::shared_ptr<data_t> data_;
-  data_t::const_iterator begin_;
-  data_t::const_iterator end_;
-  size_t size_ = 0;
-};
-
-class MyRowSequenceIterator final : public RowSequenceIterator {
-  typedef std::map<int64_t, int64_t> data_t;
-public:
-  MyRowSequenceIterator(std::shared_ptr<data_t> data,
-      data_t::const_iterator begin, data_t::const_iterator end, size_t size, bool forward);
-  ~MyRowSequenceIterator() final;
-
-  std::shared_ptr<RowSequence> getNextRowSequenceWithLength(size_t size) final;
-  bool tryGetNext(int64_t *result) final;
-
-private:
-  std::shared_ptr<data_t> data_;
-  data_t::const_iterator begin_;
-  data_t::const_iterator end_;
-  size_t size_ = 0;
-  bool forward_ = false;
-};
 }  // namespace
 
 SubscribedTableState::SubscribedTableState(
@@ -121,23 +80,26 @@ void SubscribedTableState::erase(const RowSequence &removedRows) {
 
 void SubscribedTableState::modify(std::vector<std::unique_ptr<AbstractFlexVectorBase>> modifiedData,
     const std::vector<std::shared_ptr<RowSequence>> &modifiedIndicesPerColumn) {
-  throw new std::runtime_error("TODO(kosak)");
+  throw std::runtime_error("TODO(kosak)");
 }
 
 
-void SubscribedTableState::applyShifts(const RowSequence &startIndex, const RowSequence &endInclusiveIndex,
+void SubscribedTableState::applyShifts(const RowSequence &firstIndex, const RowSequence &lastIndex,
     const RowSequence &destIndex) {
-  auto processShift = [this](int64_t s, int64_t ei, int64_t dest) {
-    mapShifter(s, ei, dest, redirection_.get());
+  auto processShift = [this](int64_t first, int64_t last, int64_t dest) {
+    uint64_t begin = first;
+    uint64_t end = ((uint64_t)last) + 1;
+    uint64_t destBegin = dest;
+    spaceMapper_.applyShift(begin, end, destBegin);
   };
-  applyShiftData(startIndex, endInclusiveIndex, destIndex, processShift);
+  applyShiftData(firstIndex, lastIndex, destIndex, processShift);
 }
 
 namespace {
-void applyShiftData(const RowSequence &startIndex, const RowSequence &endInclusiveIndex,
+void applyShiftData(const RowSequence &firstIndex, const RowSequence &lastIndex,
     const RowSequence &destIndex,
     const std::function<void(int64_t, int64_t, int64_t)> &processShift) {
-  if (startIndex.empty()) {
+  if (firstIndex.empty()) {
     return;
   }
 
@@ -146,17 +108,17 @@ void applyShiftData(const RowSequence &startIndex, const RowSequence &endInclusi
   for (auto direction = 0; direction < 2; ++direction) {
     std::shared_ptr<RowSequenceIterator> startIter, endIter, destIter;
     if (direction == 0) {
-      startIter = startIndex.getRowSequenceIterator();
-      endIter = endInclusiveIndex.getRowSequenceIterator();
+      startIter = firstIndex.getRowSequenceIterator();
+      endIter = lastIndex.getRowSequenceIterator();
       destIter = destIndex.getRowSequenceIterator();
     } else {
-      startIter = startIndex.getRowSequenceReverseIterator();
-      endIter = endInclusiveIndex.getRowSequenceReverseIterator();
+      startIter = firstIndex.getRowSequenceReverseIterator();
+      endIter = lastIndex.getRowSequenceReverseIterator();
       destIter = destIndex.getRowSequenceReverseIterator();
     }
-    int64_t start, end, dest;
-    while (startIter->tryGetNext(&start)) {
-      if (!endIter->tryGetNext(&end) || !destIter->tryGetNext(&dest)) {
+    int64_t first, last, dest;
+    while (startIter->tryGetNext(&first)) {
+      if (!endIter->tryGetNext(&last) || !destIter->tryGetNext(&dest)) {
         throw std::runtime_error("Sequences not of same size");
       }
       if (direction == 0) {
@@ -171,110 +133,56 @@ void applyShiftData(const RowSequence &startIndex, const RowSequence &endInclusi
         }
       }
       const char *dirText = direction == 0 ? "(positive)" : "(negative)";
-      streamf(std::cerr, "Processing %o shift src [%o..%o] dest %o\n", dirText, start, end, dest);
-      processShift(start, end, dest);
+      streamf(std::cerr, "Processing %o shift src [%o..%o] dest %o\n", dirText, first, last, dest);
+      processShift(first, last, dest);
     }
   }
 }
 
-void mapShifter(int64_t start, int64_t endInclusive, int64_t dest, std::map<int64_t, int64_t> *zm) {
-  auto delta = dest - start;
-  if (delta < 0) {
-    auto currentp = zm->lower_bound(start);
-    while (true) {
-      if (currentp == zm->end() || currentp->first > endInclusive) {
-        return;
-      }
-      auto nextp = std::next(currentp);
-      auto node = zm->extract(currentp);
-      auto newKey = node.key() + delta;
-      streamf(std::cerr, "Working forwards, moving key from %o to %o\n", node.key(), newKey);
-      node.key() = newKey;
-      zm->insert(std::move(node));
-      currentp = nextp;
-      ++dest;
-    }
-  }
-
-  // delta >= 0 so move in the reverse direction
-  auto currentp = zm->upper_bound(endInclusive);
-  if (currentp == zm->begin()) {
-    return;
-  }
-  --currentp;
-  while (true) {
-    if (currentp->first < start) {
-      return;
-    }
-    std::optional<std::map<int64_t, int64_t>::iterator> nextp;
-    if (currentp != zm->begin()) {
-      nextp = std::prev(currentp);
-    }
-    auto node = zm->extract(currentp);
-    auto newKey = node.key() + delta;
-    streamf(std::cerr, "Working backwards, moving key from %o to %o\n", node.key(), newKey);
-    node.key() = newKey;
-    zm->insert(std::move(node));
-    if (!nextp.has_value()) {
-      return;
-    }
-    currentp = *nextp;
-    --dest;
-  }
-}
-
-MyRowSequence::MyRowSequence(std::shared_ptr<data_t> data,
-    data_t::const_iterator begin, data_t::const_iterator end, size_t size)
-    : data_(std::move(data)), begin_(begin), end_(end), size_(size) {}
-
-std::shared_ptr<RowSequenceIterator>
-MyRowSequence::getRowSequenceIterator() const {
-  return std::make_shared<MyRowSequenceIterator>(data_, begin_, end_, size_, true);
-}
-
-std::shared_ptr<RowSequenceIterator>
-MyRowSequence::getRowSequenceReverseIterator() const {
-  return std::make_shared<MyRowSequenceIterator>(data_, begin_, end_, size_, false);
-}
-
-MyRowSequenceIterator::MyRowSequenceIterator(std::shared_ptr<data_t> data,
-    data_t::const_iterator begin, data_t::const_iterator end, size_t size, bool forward)
-    : data_(std::move(data)), begin_(begin), end_(end), size_(size), forward_(forward) {}
-
-MyRowSequenceIterator::~MyRowSequenceIterator() = default;
-
-bool MyRowSequenceIterator::tryGetNext(int64_t *result) {
-  if (begin_ == end_) {
-    return false;
-  }
-  if (forward_) {
-    *result = begin_->first;
-    ++begin_;
-  } else {
-    --end_;
-    *result = end_->first;
-  }
-  --size_;
-  return true;
-}
-
-std::shared_ptr<RowSequence>
-MyRowSequenceIterator::getNextRowSequenceWithLength(size_t size) {
-  // TODO(kosak): iterates whole set. ugh.
-  auto remaining = std::distance(begin_, end_);
-  auto sizeToUse = std::min<ssize_t>((ssize_t) size, remaining);
-  data_t::const_iterator newBegin, newEnd;
-  if (forward_) {
-    newBegin = begin_;
-    std::advance(begin_, sizeToUse);
-    newEnd = begin_;
-  } else {
-    newEnd = end_;
-    std::advance(end_, -sizeToUse);
-    newBegin = end_;
-  }
-  size_ -= sizeToUse;
-  return std::make_shared<MyRowSequence>(data_, newBegin, newEnd, sizeToUse);
-}
+//void mapShifter(int64_t start, int64_t endInclusive, int64_t dest, std::map<int64_t, int64_t> *zm) {
+//  auto delta = dest - start;
+//  if (delta < 0) {
+//    auto currentp = zm->lower_bound(start);
+//    while (true) {
+//      if (currentp == zm->end() || currentp->first > endInclusive) {
+//        return;
+//      }
+//      auto nextp = std::next(currentp);
+//      auto node = zm->extract(currentp);
+//      auto newKey = node.key() + delta;
+//      streamf(std::cerr, "Working forwards, moving key from %o to %o\n", node.key(), newKey);
+//      node.key() = newKey;
+//      zm->insert(std::move(node));
+//      currentp = nextp;
+//      ++dest;
+//    }
+//  }
+//
+//  // delta >= 0 so move in the reverse direction
+//  auto currentp = zm->upper_bound(endInclusive);
+//  if (currentp == zm->begin()) {
+//    return;
+//  }
+//  --currentp;
+//  while (true) {
+//    if (currentp->first < start) {
+//      return;
+//    }
+//    std::optional<std::map<int64_t, int64_t>::iterator> nextp;
+//    if (currentp != zm->begin()) {
+//      nextp = std::prev(currentp);
+//    }
+//    auto node = zm->extract(currentp);
+//    auto newKey = node.key() + delta;
+//    streamf(std::cerr, "Working backwards, moving key from %o to %o\n", node.key(), newKey);
+//    node.key() = newKey;
+//    zm->insert(std::move(node));
+//    if (!nextp.has_value()) {
+//      return;
+//    }
+//    currentp = *nextp;
+//    --dest;
+//  }
+//}
 }  // namespace
 }  // namespace deephaven::client::subscription
