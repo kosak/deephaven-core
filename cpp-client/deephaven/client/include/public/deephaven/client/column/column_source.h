d15d249f85dde5ecf7407282c9af3d482f1176c3
Corey Kosak <coreykosak@deephaven.io>
2022-05-26 19:19:19 -0400

    blah

--- cpp-client/deephaven/client/include/public/deephaven/client/column/column_source.h
+++ cpp-client/deephaven/client/include/public/deephaven/client/column/column_source.h
@@ -16,7 +16,7 @@ class ColumnSourceVisitor;
 // the column source interfaces
 
 class ColumnSource {
-protected:
+public:
   typedef deephaven::client::chunk::Chunk Chunk;
   typedef deephaven::client::chunk::LongChunk LongChunk;
   typedef deephaven::client::container::Context Context;
@@ -41,81 +41,39 @@ public:
 };
 
 // the per-type interfaces
-
-class IntColumnSource : public MutableColumnSource {
-public:
-  ~IntColumnSource() override;
+template<typename T>
+class NumericColumnSource : public ColumnSource {
 };
 
-class LongColumnSource : public MutableColumnSource {
-public:
-  ~LongColumnSource() override;
+// the per-type interfaces
+template<typename T>
+class MutableNumericColumnSource : public MutableColumnSource {
 };
 
-class DoubleColumnSource : public MutableColumnSource {
-public:
-  ~DoubleColumnSource() override;
-};
-
-class IntArrayColumnSource final : public IntColumnSource, std::enable_shared_from_this<IntArrayColumnSource> {
-  struct Private {};
+template<typename T>
+class NumericArrayColumnSource final : public MutableNumericColumnSource<T>,
+    std::enable_shared_from_this<NumericArrayColumnSource<T>> {
+  struct Private {
+  };
 
 public:
-    static std::shared_ptr<IntArrayColumnSource> create();
-    explicit IntArrayColumnSource(Private);
-    ~IntArrayColumnSource() final;
-
-    std::shared_ptr<ColumnSourceContext> createContext(size_t chunkSize) const final;
-    void fillChunk(Context *context, const RowSequence &rows, Chunk *dest) const final;
-    void fillChunkUnordered(Context *context, const LongChunk &rowKeys, size_t size, Chunk *dest) const final;
-    void fillFromChunk(Context *context, const Chunk &src, const RowSequence &rows) final;
-    void fillFromChunkUnordered(Context *context, const Chunk &src, const LongChunk &rowKeys, size_t size) final;
-
-    void acceptVisitor(ColumnSourceVisitor *visitor) const final;
-
-private:
-    void ensureSize(size_t size);
-    std::vector<int32_t> data_;
-};
-
-class LongArrayColumnSource final : public LongColumnSource, std::enable_shared_from_this<LongArrayColumnSource> {
-  struct Private {};
-public:
-  static std::shared_ptr<LongArrayColumnSource> create();
-  explicit LongArrayColumnSource(Private);
-  ~LongArrayColumnSource() final;
+  static std::shared_ptr<NumericArrayColumnSource> create();
+  explicit NumericArrayColumnSource(Private);
+  ~NumericArrayColumnSource() final;
 
   std::shared_ptr<ColumnSourceContext> createContext(size_t chunkSize) const final;
   void fillChunk(Context *context, const RowSequence &rows, Chunk *dest) const final;
-  void fillChunkUnordered(Context *context, const LongChunk &rowKeys, size_t size, Chunk *dest) const final;
+  void fillChunkUnordered(Context *context, const LongChunk &rowKeys, size_t size,
+      Chunk *dest) const final;
   void fillFromChunk(Context *context, const Chunk &src, const RowSequence &rows) final;
-  void fillFromChunkUnordered(Context *context, const Chunk &src, const LongChunk &rowKeys, size_t size) final;
+  void fillFromChunkUnordered(Context *context, const Chunk &src, const LongChunk &rowKeys,
+      size_t size) final;
 
   void acceptVisitor(ColumnSourceVisitor *visitor) const final;
 
 private:
   void ensureSize(size_t size);
-  std::vector<int64_t> data_;
-};
-
-class DoubleArrayColumnSource final : public DoubleColumnSource, std::enable_shared_from_this<DoubleArrayColumnSource> {
-  struct Private {};
-public:
-  static std::shared_ptr<DoubleArrayColumnSource> create();
-  explicit DoubleArrayColumnSource(Private);
-  ~DoubleArrayColumnSource() final;
-
-  std::shared_ptr<ColumnSourceContext> createContext(size_t chunkSize) const final;
-  void fillChunk(Context *context, const RowSequence &rows, Chunk *dest) const final;
-  void fillChunkUnordered(Context *context, const LongChunk &rowKeys, size_t size, Chunk *dest) const final;
-  void fillFromChunk(Context *context, const Chunk &src, const RowSequence &rows) final;
-  void fillFromChunkUnordered(Context *context, const Chunk &src, const LongChunk &rowKeys, size_t size) final;
-
-  void acceptVisitor(ColumnSourceVisitor *visitor) const final;
-
-private:
-  void ensureSize(size_t size);
-  std::vector<double> data_;
+  std::vector<int32_t> data_;
 };
 
 class ColumnSourceContext : public deephaven::client::container::Context {
--- cpp-client/deephaven/client/src/immerutil/immer_column_source.cc
+++ cpp-client/deephaven/client/src/immerutil/immer_column_source.cc
@@ -1,11 +1,19 @@
 #include "deephaven/client/immerutil/immer_column_source.h"
 
 #include "deephaven/client/utility/utility.h"
+#include "deephaven/client/column/column_source.h"
+using deephaven::client::column::ColumnSourceContext;
 using deephaven::client::utility::stringf;
 
 namespace deephaven::client::immerutil {
-auto ImmerColumnSourceBase::createContext(size_t chunkSize) const -> std::shared_ptr<ColumnSourceContext> {
-  throw std::runtime_error(stringf("TODO(kosak): %o", __PRETTY_FUNCTION__));
+namespace {
+class MyContext : public ColumnSourceContext {
+
+};
+
+}  // namespace
+std::shared_ptr<ColumnSourceContext> ImmerColumnSourceBase::createContext(size_t chunkSize) const {
+  return std::make_shared<MyContext>();
 }
 
 void ImmerColumnSourceBase::fillChunkUnordered(Context *context, const LongChunk &rowKeys, size_t size,
