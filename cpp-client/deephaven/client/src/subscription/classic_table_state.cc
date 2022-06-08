88b2823194aa7bca13ee3a38e7efe7472d22c5c7
Corey Kosak <coreykosak@deephaven.io>
2022-06-08 19:21:24 -0400

    ugh

--- cpp-client/deephaven/client/src/subscription/classic_table_state.cc
+++ cpp-client/deephaven/client/src/subscription/classic_table_state.cc
@@ -5,15 +5,27 @@
 #include "deephaven/client/subscription/shift_processor.h"
 #include "deephaven/client/utility/utility.h"
 
+using deephaven::client::column::ColumnSource;
+using deephaven::client::column::NumericArrayColumnSource;
 using deephaven::client::container::RowSequence;
 using deephaven::client::container::RowSequenceBuilder;
+using deephaven::client::utility::ColumnDefinitions;
+using deephaven::client::utility::okOrThrow;
 using deephaven::client::utility::streamf;
 using deephaven::client::utility::stringf;
 
 namespace deephaven::client::subscription {
 namespace {
 void mapShifter(uint64_t begin, uint64_t end, uint64_t dest, std::map<uint64_t, uint64_t> *map);
+
+std::vector<std::shared_ptr<ColumnSource>> makeColumnSources(const ColumnDefinitions &colDefs);
 }
+
+ClassicTableState::ClassicTableState(const ColumnDefinitions &colDefs) {
+  columns_ = makeColumnSources(colDefs);
+}
+
+ClassicTableState::~ClassicTableState() = default;
 
 std::shared_ptr<RowSequence> ClassicTableState::erase(const RowSequence &rowsToRemoveKeySpace) {
   RowSequenceBuilder resultBuilder;
@@ -133,6 +145,36 @@ void mapShifter(uint64_t begin, uint64_t end, uint64_t dest, std::map<uint64_t, 
     }
     currentp = nextp;
   }
+}
+
+struct MyVisitor final : public arrow::TypeVisitor {
+  arrow::Status Visit(const arrow::Int32Type &type) final {
+    columnSource_ = NumericArrayColumnSource<int32_t>::create();
+    return arrow::Status::OK();
+  }
+
+  arrow::Status Visit(const arrow::Int64Type &type) final {
+    columnSource_ = NumericArrayColumnSource<int64_t>::create();
+    return arrow::Status::OK();
+  }
+
+  arrow::Status Visit(const arrow::DoubleType &type) final {
+    columnSource_ = NumericArrayColumnSource<double>::create();
+    return arrow::Status::OK();
+  }
+
+  std::shared_ptr<ColumnSource> columnSource_;
+};
+
+std::vector<std::shared_ptr<ColumnSource>> makeColumnSources(const ColumnDefinitions &colDefs) {
+  std::vector<std::shared_ptr<ColumnSource>> result;
+  for (const auto &[name, arrowType] : colDefs.vec()) {
+    MyVisitor v;
+    okOrThrow(DEEPHAVEN_EXPR_MSG(arrowType->Accept(&v)));
+    result.push_back(v.columnSource_);
+  }
+
+  return result;
 }
 }  // namespace
 }  // namespace deephaven::client::subscription
