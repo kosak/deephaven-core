08664070eb2b93a72309ee685012a62c331fb01d
Corey Kosak <kosak@kosak.com>
2024-04-30 15:17:20 -0400

    something like that

--- csharp/client/DhClient/Interop/BasicInteropInteractions.cs
+++ csharp/client/DhClient/Interop/BasicInteropInteractions.cs
@@ -18,5 +18,36 @@ public class BasicInteropInteractions {
   public static extern void deephaven_dhcore_basicInteropInteractions_Add(Int32 a, Int32 b, out Int32 result);
 
   [DllImport(Constants.DhCorePath, CharSet = CharSet.Unicode)]
+  public static extern void deephaven_dhcore_basicInteropInteractions_AddInPlace(ref Int32 value, Int32 offset);
+
+  [DllImport(Constants.DhCorePath, CharSet = CharSet.Unicode)]
   public static extern void deephaven_dhcore_basicInteropInteractions_Concat(string a, string b, out string result);
+
+  [DllImport(Constants.DhCorePath, CharSet = CharSet.Unicode)]
+  public static extern void deephaven_dhcore_basicInteropInteractions_ConcatInPlace(ref string s, string toAppend);
+
+  [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+  public struct BasicStruct {
+    public int i;
+    public string s;
+  }
+
+  [DllImport(Constants.DhCorePath, CharSet = CharSet.Unicode)]
+  public static extern void deephaven_dhcore_basicInteropInteractions_SetBasicStruct(
+    int i, string s, int iOffset, string sAppend, out BasicStruct result);
+
+  [DllImport(Constants.DhCorePath, CharSet = CharSet.Unicode)]
+  public static extern void deephaven_dhcore_basicInteropInteractions_SetBasicStructInPlace(
+    ref BasicStruct s, int iOffset, string sAppend);
+
+  [DllImport(Constants.DhCorePath, CharSet = CharSet.Unicode)]
+  public static extern void deephaven_dhcore_basicInteropInteractions_RunningSum(
+    [In, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 2)] Int32[] data,
+    [Out, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 2)] Int32[] result,
+    Int32 length);
+
+  [DllImport(Constants.DhCorePath, CharSet = CharSet.Unicode)]
+  public static extern void deephaven_dhcore_basicInteropInteractions_RunningSumInPlace(
+    [In, Out, MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 1)] Int32[] data,
+    Int32 length);
 }
--- csharp/client/DhClientTests/BasicInteropTest.cs
+++ csharp/client/DhClientTests/BasicInteropTest.cs
@@ -5,10 +5,20 @@ namespace Deephaven.DhClientTests;
 public class BasicInteropTest {
   [Fact]
   public void TestInteropInteractions() {
-    BasicInteropInteractions.deephaven_dhcore_basicInteropInteractions_Add(3, 4, out var result1);
-    Assert.Equal(7, result1);
+    {
+      BasicInteropInteractions.deephaven_dhcore_basicInteropInteractions_Add(3, 4, out var result);
+      Assert.Equal(7, result);
+    }
 
-    BasicInteropInteractions.deephaven_dhcore_basicInteropInteractions_Concat("Deep", "haven", out var result2);
-    Assert.Equal("Deephaven", result2);
+    {
+      BasicInteropInteractions.deephaven_dhcore_basicInteropInteractions_Concat("Deep", "haven", out var result);
+      Assert.Equal("Deephaven", result);
+    }
+
+    {
+      int[] data = { 10, 20, 30, 40, 50 };
+
+      int[] result = { 10, 20, 30, 40, 50 };
+      BasicInteropInteractions.deephaven_dhcore_basicInteropInteractions_Add(3, 4, out var result1);
+    }
   }
-}
