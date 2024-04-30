92cdb938e60883c7b445f1cefca83a830af26309
Corey Kosak <kosak@kosak.com>
2024-04-30 14:58:24 -0400

    I'm kind of ok with this

---
+++ csharp/client/DhClient/Interop/BasicInteropInteractions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace Deephaven.DhClient.Interop;
+
+class Constants {
+  public const string DhCorePath = "dhcore.dll";
+  public const string DhClientPath = "dhclient.dll";
+}
+
+public class BasicInteropInteractions {
+  [DllImport(Constants.DhCorePath, CharSet = CharSet.Unicode)]
+  public static extern void deephaven_dhcore_basicInteropInteractions_Add(Int32 a, Int32 b, out Int32 result);
+
+  [DllImport(Constants.DhCorePath, CharSet = CharSet.Unicode)]
+  public static extern void deephaven_dhcore_basicInteropInteractions_Concat(string a, string b, out string result);
+}
--- csharp/client/DhClientTests/BasicInteropTest.cs
+++ csharp/client/DhClientTests/BasicInteropTest.cs
@@ -1,8 +1,14 @@
+using Deephaven.DhClient.Interop;
+
 namespace Deephaven.DhClientTests;
 
 public class BasicInteropTest {
   [Fact]
-  public void Test1() {
+  public void TestInteropInteractions() {
+    BasicInteropInteractions.deephaven_dhcore_basicInteropInteractions_Add(3, 4, out var result1);
+    Assert.Equal(7, result1);
 
+    BasicInteropInteractions.deephaven_dhcore_basicInteropInteractions_Concat("Deep", "haven", out var result2);
+    Assert.Equal("Deephaven", result2);
   }
 }
