b3c2d142a288d44e5146195355631fb4bafd6688
Corey Kosak <kosak@kosak.com>
2024-05-17 19:27:53 -0400

    I think I am "ok" with this approach

--- csharp/client/DeephavenClient/Utility/ColumnFactory.cs
+++ csharp/client/DeephavenClient/Utility/ColumnFactory.cs
@@ -3,52 +3,83 @@ using System;
 
 namespace Deephaven.DeephavenClient.Utility;
 
-internal enum ColumnFactoryMode {
-  DataAndNullArray,
-  ArrayOfNullables
-}
-
 internal abstract class ColumnFactory<TTableType> {
+  public abstract (Array, bool[]) GetColumn(NativePtr<TTableType> table, Int32 columnIndex,
+    Int64 numRows);
 
-  public abstract (Array, bool[]?) GetColumn(NativePtr<TTableType> table, Int32 columnIndex,
-    Int64 numRows, ColumnFactoryMode mode);
+  public abstract (Array, bool[]) GetNullableColumn(NativePtr<TTableType> table, Int32 columnIndex,
+    Int64 numRows);
 
   public delegate void NativeImpl<in T>(NativePtr<TTableType> table, Int32 columnIndex,
     T[] data, sbyte[]? nullFlags, Int64 numRows, out ErrorStatus status);
 
-  public sealed class ForGeneric<T> : ColumnFactory<TTableType> {
-    private readonly NativeImpl<T> _nativeImpl;
+  public sealed class ForString : ColumnFactory<TTableType> {
+    private readonly NativeImpl<string> _nativeImpl;
 
-    public ForGeneric(NativeImpl<T> nativeImpl) => _nativeImpl = nativeImpl;
+    public ForString(NativeImpl<string> nativeImpl) => _nativeImpl = nativeImpl;
 
-    public override (Array, bool[]?) GetColumn(NativePtr<TTableType> table, Int32 columnIndex,
-      Int64 numRows, ColumnFactoryMode mode) {
-      var data = new T[numRows];
+    public override (Array, bool[]) GetColumn(NativePtr<TTableType> table, Int32 columnIndex,
+      Int64 numRows) {
+      var data = new string[numRows];
       var nullsAsSbytes = new sbyte[numRows];
       _nativeImpl(table, columnIndex, data, nullsAsSbytes, numRows, out var errorStatus);
       errorStatus.OkOrThrow();
+      var nulls = new bool[numRows];
+      for (Int64 i = 0; i < numRows; ++i) {
+        nulls[i] = nullsAsSbytes[i] != 0;
+      }
 
-      return Adapt(data, nullsAsSbytes, mode);
+      return (data, nulls);
+    }
+
+    public override (Array, bool[]) GetNullableColumn(NativePtr<TTableType> table, int columnIndex, long numRows) {
+      // string is a reference type so there's no such thing as a Nullable<string>.
+      // For the case of string, the return value is the same as GetColumn().
+      return GetColumn(table, columnIndex, numRows);
     }
   }
 
+  
   public sealed class ForBool : ColumnFactory<TTableType> {
     private readonly NativeImpl<sbyte> _nativeImpl;
 
     public ForBool(NativeImpl<sbyte> nativeImpl) => _nativeImpl = nativeImpl;
 
-    public override (Array, bool[]?) GetColumn(NativePtr<TTableType> table, Int32 columnIndex,
-      Int64 numRows, ColumnFactoryMode mode) {
+    public override (Array, bool[]) GetColumn(NativePtr<TTableType> table, Int32 columnIndex,
+      Int64 numRows) {
       var intermediate = new sbyte[numRows];
       var nullsAsSbytes = new sbyte[numRows];
       _nativeImpl(table, columnIndex, intermediate, nullsAsSbytes, numRows, out var errorStatus);
       errorStatus.OkOrThrow();
       var data = new bool[numRows];
+      var nulls = new bool[numRows];
       for (Int64 i = 0; i < numRows; ++i) {
         data[i] = intermediate[i] != 0;
+        nulls[i] = nullsAsSbytes[i] != 0;
       }
-      return Adapt(data, nullsAsSbytes, mode);
+      return (data, nulls);
     }
+
+    public override (Array, bool[]) GetNullableColumn(NativePtr<TTableType> table, int columnIndex, long numRows) {
+      var (data, bools) = GetColumnInternal(table, columnIndex, numRows);
+      return Adapt(data, bools);
+    }
+
+    private (bool[], bool[]) GetColumnInternal(NativePtr<TTableType> table, Int32 columnIndex,
+      Int64 numRows) {
+      var intermediate = new sbyte[numRows];
+      var nullsAsSbytes = new sbyte[numRows];
+      _nativeImpl(table, columnIndex, intermediate, nullsAsSbytes, numRows, out var errorStatus);
+      errorStatus.OkOrThrow();
+      var data = new bool[numRows];
+      var nulls = new bool[numRows];
+      for (Int64 i = 0; i < numRows; ++i) {
+        data[i] = intermediate[i] != 0;
+        nulls[i] = nullsAsSbytes[i] != 0;
+      }
+      return (data, nulls);
+    }
+
   }
 
   public sealed class ForDateTime : ColumnFactory<TTableType> {
@@ -57,21 +88,52 @@ internal abstract class ColumnFactory<TTableType> {
     public ForDateTime(NativeImpl<Int64> nativeImpl) => _nativeImpl = nativeImpl;
 
     public override (Array, bool[]?) GetColumn(NativePtr<TTableType> table, Int32 columnIndex,
-      Int64 numRows, ColumnFactoryMode mode) {
+      Int64 numRows) {
       var intermediate = new Int64[numRows];
       var nullsAsSbytes = new sbyte[numRows];
       _nativeImpl(table, columnIndex, intermediate, nullsAsSbytes, numRows, out var errorStatus);
       errorStatus.OkOrThrow();
       var data = new DateTime[numRows];
+      var nulls = new bool[numRows];
       for (Int64 i = 0; i < numRows; ++i) {
         var micros = intermediate[i] / 1000;
         data[i] = DateTimeOffset.UnixEpoch.AddMicroseconds(micros).DateTime;
+        nulls[i] = nullsAsSbytes[i] != 0;
       }
-      return Adapt(data, nullsAsSbytes, mode);
+      return (data, nulls);
+    }
+
+    public override (Array, bool[]?) GetNullableColumn(NativePtr<TTableType> table, int columnIndex, long numRows) {
+      var (data, bools) = GetColumn(table, columnIndex, numRows);
+      return Adapt(data, bools);
     }
   }
 
-  private static (Array, bool[]?) Adapt<T>(T[] data, sbyte[]? nullsAsSbytes, ColumnFactoryMode mode) {
+  public sealed class ForOtherValueType<T> : ColumnFactory<TTableType> where T : struct {
+    private readonly NativeImpl<T> _nativeImpl;
+
+    public ForOtherValueType(NativeImpl<T> nativeImpl) => _nativeImpl = nativeImpl;
+
+    public override (Array, bool[]) GetColumn(NativePtr<TTableType> table, Int32 columnIndex,
+      Int64 numRows) {
+      var data = new T[numRows];
+      var nullsAsSbytes = new sbyte[numRows];
+      _nativeImpl(table, columnIndex, data, nullsAsSbytes, numRows, out var errorStatus);
+      errorStatus.OkOrThrow();
+      var nulls = new bool[numRows];
+      for (Int64 i = 0; i < numRows; ++i) {
+        nulls[i] = nullsAsSbytes[i] != 0;
+      }
+      return (data, nulls);
+    }
+
+    public override (Array, bool[]) GetNullableColumn(NativePtr<TTableType> table, int columnIndex, long numRows) {
+      var (data, bools) = GetColumn(table, columnIndex, numRows);
+      return Adapt(data, bools);
+    }
+  }
+
+  private static (Array, bool[]) Adapt<T>(T[] data, sbyte[] nullsAsSbytes) {
     var numRows = data.Length;
 
     if (mode == ColumnFactoryMode.DataAndNullArray) {
@@ -89,7 +151,7 @@ internal abstract class ColumnFactory<TTableType> {
 
     var nullableData = new T?[numRows];
     for (Int64 i = 0; i != numRows; ++i) {
-      if (nullsAsSbytes![i] != 0) {
+      if (nullsAsSbytes![i] == 0) {
         nullableData[i] = data[i];
       }
     }
