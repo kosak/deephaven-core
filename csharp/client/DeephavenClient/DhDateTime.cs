319c42fcb48cb235cfb1e1b7bdd08ed43c848055
Corey Kosak <kosak@kosak.com>
2024-05-19 15:54:44 -0400

    tpnn

---
+++ csharp/client/DeephavenClient/DhDateTime.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deephaven.DeephavenClient;
+
+/// <summary>
+/// Deephaven's custom representation of DateTime, with full nanosecond resolution,
+/// unlike .NET's own System.DateTime which has 100ns resolution.
+/// </summary>
+public class DhDateTime {
+  public readonly Int64 Nanos;
+
+  public DhDateTime(Int64 nanos) => Nanos = nanos;
+
+  public DateTime DateTime => DateTime.UnixEpoch.AddTicks(Nanos / 100);
+}
