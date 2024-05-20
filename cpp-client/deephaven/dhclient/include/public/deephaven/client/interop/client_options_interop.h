5b048ce160623cfcd702fcb41a1fcf359872f28c
Corey Kosak <coreykosak@deephaven.io>
2024-05-20 13:13:48 -0400

    ok these are less broken than before

--- cpp-client/deephaven/dhclient/include/public/deephaven/client/interop/client_options_interop.h
+++ cpp-client/deephaven/dhclient/include/public/deephaven/client/interop/client_options_interop.h
@@ -6,32 +6,52 @@
 #include "deephaven/dhcore/interop/interop_util.h"
 
 extern "C" {
-void deephaven_client_ClientOptions_ctor(deephaven::client::ClientOptions **result,
-    deephaven::dhcore::interop::ErrorStatus *status);
-void deephaven_client_ClientOptions_dtor(deephaven::client::ClientOptions *self);
-void deephaven_client_ClientOptions_SetDefaultAuthentication(deephaven::client::ClientOptions *self,
-    deephaven::dhcore::interop::ErrorStatus *status);
-void deephaven_client_ClientOptions_SetBasicAuthentication(deephaven::client::ClientOptions *self,
+void deephaven_client_ClientOptions_ctor(
+    deephaven::dhcore::interop::NativePtr<deephaven::client::ClientOptions> *result,
+    deephaven::dhcore::interop::ErrorStatusNew *status);
+void deephaven_client_ClientOptions_dtor(
+    deephaven::dhcore::interop::NativePtr<deephaven::client::ClientOptions> self);
+void deephaven_client_ClientOptions_SetDefaultAuthentication(
+    deephaven::dhcore::interop::NativePtr<deephaven::client::ClientOptions> self,
+    deephaven::dhcore::interop::ErrorStatusNew *status);
+void deephaven_client_ClientOptions_SetBasicAuthentication(
+    deephaven::dhcore::interop::NativePtr<deephaven::client::ClientOptions> self,
     const char *username, const char *password,
-    deephaven::dhcore::interop::ErrorStatus *status);
-void deephaven_client_ClientOptions_SetCustomAuthentication(deephaven::client::ClientOptions *self,
+    deephaven::dhcore::interop::ErrorStatusNew *status);
+void deephaven_client_ClientOptions_SetCustomAuthentication(
+    deephaven::dhcore::interop::NativePtr<deephaven::client::ClientOptions> self,
     const char *authentication_key, const char *authentication_value,
-    deephaven::dhcore::interop::ErrorStatus *status);
-void deephaven_client_ClientOptions_SetSessionType(deephaven::client::ClientOptions *self,
-    const char *session_type, deephaven::dhcore::interop::ErrorStatus *status);
-void deephaven_client_ClientOptions_SetUseTls(deephaven::client::ClientOptions *self,
-    bool use_tls, deephaven::dhcore::interop::ErrorStatus *status);
-void deephaven_client_ClientOptions_SetTlsRootCerts(deephaven::client::ClientOptions *self,
-    const char *tls_root_certs, deephaven::dhcore::interop::ErrorStatus *status);
-void deephaven_client_ClientOptions_SetClientCertChain(deephaven::client::ClientOptions *self,
-    const char *client_cert_chain, deephaven::dhcore::interop::ErrorStatus *status);
-void deephaven_client_ClientOptions_SetClientPrivateKey(deephaven::client::ClientOptions *self,
-    const char *client_private_key, deephaven::dhcore::interop::ErrorStatus *status);
-void deephaven_client_ClientOptions_AddIntOption(deephaven::client::ClientOptions *self,
-    const char *opt, int32_t val, deephaven::dhcore::interop::ErrorStatus *status);
-void deephaven_client_ClientOptions_AddStringOption(deephaven::client::ClientOptions *self,
-    const char *opt, const char *val, deephaven::dhcore::interop::ErrorStatus *status);
-void deephaven_client_ClientOptions_AddExtraHeader(deephaven::client::ClientOptions *self,
+    deephaven::dhcore::interop::ErrorStatusNew *status);
+void deephaven_client_ClientOptions_SetSessionType(
+    deephaven::dhcore::interop::NativePtr<deephaven::client::ClientOptions> self,
+    const char *session_type,
+    deephaven::dhcore::interop::ErrorStatusNew *status);
+void deephaven_client_ClientOptions_SetUseTls(
+    deephaven::dhcore::interop::NativePtr<deephaven::client::ClientOptions> self,
+    bool use_tls,
+    deephaven::dhcore::interop::ErrorStatusNew *status);
+void deephaven_client_ClientOptions_SetTlsRootCerts(
+    deephaven::dhcore::interop::NativePtr<deephaven::client::ClientOptions> self,
+    const char *tls_root_certs,
+    deephaven::dhcore::interop::ErrorStatusNew *status);
+void deephaven_client_ClientOptions_SetClientCertChain(
+    deephaven::dhcore::interop::NativePtr<deephaven::client::ClientOptions> self,
+    const char *client_cert_chain,
+    deephaven::dhcore::interop::ErrorStatusNew *status);
+void deephaven_client_ClientOptions_SetClientPrivateKey(
+    deephaven::dhcore::interop::NativePtr<deephaven::client::ClientOptions> self,
+    const char *client_private_key,
+    deephaven::dhcore::interop::ErrorStatusNew *status);
+void deephaven_client_ClientOptions_AddIntOption(
+    deephaven::dhcore::interop::NativePtr<deephaven::client::ClientOptions> self,
+    const char *opt, int32_t val,
+    deephaven::dhcore::interop::ErrorStatusNew *status);
+void deephaven_client_ClientOptions_AddStringOption(
+    deephaven::dhcore::interop::NativePtr<deephaven::client::ClientOptions> self,
+    const char *opt, const char *val,
+    deephaven::dhcore::interop::ErrorStatusNew *status);
+void deephaven_client_ClientOptions_AddExtraHeader(
+    deephaven::dhcore::interop::NativePtr<deephaven::client::ClientOptions> self,
     const char *header_name, const char *header_value,
-    deephaven::dhcore::interop::ErrorStatus *status);
+    deephaven::dhcore::interop::ErrorStatusNew *status);
 }  // extern "C"
--- cpp-client/deephaven/dhclient/src/interop/client_options_interop.cc
+++ cpp-client/deephaven/dhclient/src/interop/client_options_interop.cc
@@ -7,106 +7,109 @@
 #include "deephaven/client/client_options.h"
 #include "deephaven/dhcore/interop/interop_util.h"
 
+using deephaven::dhcore::interop::ErrorStatusNew;
+using deephaven::dhcore::interop::NativePtr;
+
 using deephaven::client::ClientOptions;
 
 extern "C" {
-void deephaven_client_ClientOptions_ctor(ClientOptions **result,
-    deephaven::dhcore::interop::ErrorStatus *status) {
+void deephaven_client_ClientOptions_ctor(NativePtr<ClientOptions> *result,
+    ErrorStatusNew *status) {
   status->Run([=]() {
-    *result = new ClientOptions();
+    result->Reset(new ClientOptions());
   });
 }
 
-void deephaven_client_ClientOptions_dtor(ClientOptions *self) {
+void deephaven_client_ClientOptions_dtor(NativePtr<ClientOptions> self) {
   delete self;
 }
 
-void deephaven_client_ClientOptions_SetDefaultAuthentication(ClientOptions *self,
-    deephaven::dhcore::interop::ErrorStatus *status) {
+void deephaven_client_ClientOptions_SetDefaultAuthentication(NativePtr<ClientOptions> self,
+    ErrorStatusNew *status) {
   status->Run([=]() {
     self->SetDefaultAuthentication();
   });
 }
 
-void deephaven_client_ClientOptions_SetBasicAuthentication(ClientOptions *self,
+void deephaven_client_ClientOptions_SetBasicAuthentication(NativePtr<ClientOptions> self,
     const char *username, const char *password,
-    deephaven::dhcore::interop::ErrorStatus *status) {
+    ErrorStatusNew *status) {
   status->Run([=]() {
     self->SetBasicAuthentication(username, password);
   });
 }
 
-void deephaven_client_ClientOptions_SetCustomAuthentication(ClientOptions *self,
+void deephaven_client_ClientOptions_SetCustomAuthentication(NativePtr<ClientOptions> self,
     const char *authentication_key, const char *authentication_value,
-    deephaven::dhcore::interop::ErrorStatus *status) {
+    ErrorStatusNew *status) {
   status->Run([=]() {
     self->SetCustomAuthentication(authentication_key, authentication_value);
   });
 }
 
-void deephaven_client_ClientOptions_SetSessionType(ClientOptions *self,
+void deephaven_client_ClientOptions_SetSessionType(NativePtr<ClientOptions> self,
     const char *session_type,
-    deephaven::dhcore::interop::ErrorStatus *status) {
+    ErrorStatusNew *status) {
   status->Run([=]() {
     self->SetSessionType(session_type);
   });
 }
 
-void deephaven_client_ClientOptions_SetUseTls(ClientOptions *self,
+void deephaven_client_ClientOptions_SetUseTls(NativePtr<ClientOptions> self,
     bool use_tls,
-    deephaven::dhcore::interop::ErrorStatus *status) {
+    ErrorStatusNew *status) {
   status->Run([=]() {
     self->SetUseTls(use_tls);
   });
 
 }
 
-void deephaven_client_ClientOptions_SetTlsRootCerts(ClientOptions *self,
+void deephaven_client_ClientOptions_SetTlsRootCerts(NativePtr<ClientOptions> self,
     const char *tls_root_certs,
-    deephaven::dhcore::interop::ErrorStatus *status) {
+    ErrorStatusNew *status) {
   status->Run([=]() {
     self->SetTlsRootCerts(tls_root_certs);
   });
 }
 
-void deephaven_client_ClientOptions_SetClientCertChain(ClientOptions *self,
+void deephaven_client_ClientOptions_SetClientCertChain(NativePtr<ClientOptions> self,
     const char *client_cert_chain,
-    deephaven::dhcore::interop::ErrorStatus *status) {
+    ErrorStatusNew *status) {
   status->Run([=]() {
     self->SetClientCertChain(client_cert_chain);
   });
 
 }
 
-void deephaven_client_ClientOptions_SetClientPrivateKey(ClientOptions *self,
+void deephaven_client_ClientOptions_SetClientPrivateKey(NativePtr<ClientOptions> self,
     const char *client_private_key,
-    deephaven::dhcore::interop::ErrorStatus *status) {
+    ErrorStatusNew *status) {
   status->Run([=]() {
     self->SetClientPrivateKey(client_private_key);
   });
 
 }
 
-void deephaven_client_ClientOptions_AddIntOption(ClientOptions *self,
+void deephaven_client_ClientOptions_AddIntOption(NativePtr<ClientOptions> self,
     const char *opt, int32_t val,
-    deephaven::dhcore::interop::ErrorStatus *status) {
+    ErrorStatusNew *status) {
   status->Run([=]() {
     self->AddIntOption(opt, val);
   });
 
 }
 
-void deephaven_client_ClientOptions_AddStringOption(ClientOptions *self,
+void deephaven_client_ClientOptions_AddStringOption(NativePtr<ClientOptions> self,
     const char *opt, const char *val,
-    deephaven::dhcore::interop::ErrorStatus *status) {
+    ErrorStatusNew *status) {
   status->Run([=]() {
     self->AddStringOption(opt, val);
   });
 }
 
-void deephaven_client_ClientOptions_AddExtraHeader(ClientOptions *self,
+void deephaven_client_ClientOptions_AddExtraHeader(NativePtr<ClientOptions> self,
     const char *header_name, const char *header_value,
-    deephaven::dhcore::interop::ErrorStatus *status) {
+    ErrorStatusNew *status) {
   status->Run([=]() {
     self->AddExtraHeader(header_name, header_value);
   });
