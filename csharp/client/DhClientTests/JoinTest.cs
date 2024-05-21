1df6e5d14660bc03c69a417a5c60465a090b5dd3
Corey Kosak <kosak@kosak.com>
2024-05-21 19:19:48 -0400

    what happened here

--- csharp/client/DhClientTests/JoinTest.cs
+++ csharp/client/DhClientTests/JoinTest.cs
@@ -1,4 +1,6 @@
 ﻿using Deephaven.DeephavenClient;
+using Deephaven.DeephavenClient.Utility;
+using System;
 using Xunit.Abstractions;
 
 namespace Deephaven.DhClientTests;
@@ -16,13 +18,12 @@ public class JoinTest {
     var tm = ctx.Client.Manager;
     var testTable = ctx.TestTable;
 
-    var table = testTable.Where("ImportDate == `2017-11-01`");
-    var lastClose = table.LastBy("Ticker");
-    var avgView = table.View("Ticker", "Volume").AvgBy("Ticker");
+    using var table = testTable.Where("ImportDate == `2017-11-01`");
+    using var lastClose = table.LastBy("Ticker");
+    using var avgView = table.View("Ticker", "Volume").AvgBy("Ticker");
 
-    var joined = lastClose.NaturalJoin(avgView, new[] {"Ticker"}, new[]{ "ADV = Volume"});
-
-    var filtered = joined.Select("Ticker", "Close", "ADV");
+    using var joined = lastClose.NaturalJoin(avgView, new[] {"Ticker"}, new[]{ "ADV = Volume"});
+    using var filtered = joined.Select("Ticker", "Close", "ADV");
 
     var tickerData = new[] { "XRX", "XYZZY", "IBM", "GME", "AAPL", "ZNGA" };
     var closeData = new[] { 53.8, 88.5, 38.7, 453, 26.7, 544.9 };
@@ -33,6 +34,98 @@ public class JoinTest {
     tc.AddColumn("Close", closeData);
     tc.AddColumn("ADV", advData);
     tc.AssertEqualTo(filtered);
+  }
+
+  [Fact]
+  public void TestAj() {
+    using var ctx = CommonContextForTests.Create(new ClientOptions());
+    var tm = ctx.Client.Manager;
+    var testTable = ctx.TestTable;
+
+    TableHandle trades;
+    {
+      var tickerData = new[] { "AAPL", "AAPL", "AAPL", "IBM", "IBM" };
+      var instantDdata = new[] {
+        DhDateTime.Parse("2021-04-05T09:10:00-0500"),
+        DhDateTime.Parse("2021-04-05T09:31:00-0500"),
+        DhDateTime.Parse("2021-04-05T16:00:00-0500"),
+        DhDateTime.Parse("2021-04-05T16:00:00-0500"),
+        DhDateTime.Parse("2021-04-05T16:30:00-0500")
+      };
+      var priceData = new[] { 2.5, 3.7, 3.0, 100.50, 110 };
+      var sizeData = new[] { 52, 14, 73, 11, 6 };
+      using var tableMaker = new TableMaker();
+      tableMaker.AddColumn("Ticker", tickerData);
+      tableMaker.AddColumn("Timestamp", instantDdata);
+      tableMaker.AddColumn("Price", priceData);
+      tableMaker.AddColumn("Size", sizeData);
+      trades = tableMaker.MakeTable(tm);
+    }
+
+    TableHandle quotes;
+    {
+      var tickerData = new[] { "AAPL", "AAPL", "IBM", "IBM", "IBM" };
+      var timeStampData = new[] {
+        DhDateTime.Parse("2021-04-05T09:11:00-0500"),
+        DhDateTime.Parse("2021-04-05T09:30:00-0500"),
+        DhDateTime.Parse("2021-04-05T16:00:00-0500"),
+        DhDateTime.Parse("2021-04-05T16:30:00-0500"),
+        DhDateTime.Parse("2021-04-05T17:00:00-0500")
+      };
+      var bidData = new[] { 2.5, 3.4, 97, 102, 108 };
+      var bidSizeData = new[] { 10, 20, 5, 13, 23 };
+      var askData = new[] { 2.5, 3.4, 105, 110, 111 };
+      var askSizeData = new[] { 83, 33, 47, 15, 5 };
+      var tableMaker = new TableMaker();
+      tableMaker.AddColumn("Ticker", tickerData);
+      tableMaker.AddColumn("Timestamp", timeStampData);
+      tableMaker.AddColumn("Bid", bidData);
+      tableMaker.AddColumn("BidSize", bidSizeData);
+      tableMaker.AddColumn("Ask", askData);
+      tableMaker.AddColumn("AskSize", askSizeData);
+      quotes = tableMaker.MakeTable(tm);
+    }
+
+    using var result = trades.Aj(quotes, new[] { "Ticker", "Timestamp" });
+
+    // Expected data
+    {
+      var tickerData = new[] { "AAPL", "AAPL", "AAPL", "IBM", "IBM" };
+      var timestampData = new[] {
+        DhDateTime.Parse("2021-04-05T09:10:00-0500"),
+        DhDateTime.Parse("2021-04-05T09:31:00-0500"),
+        DhDateTime.Parse("2021-04-05T16:00:00-0500"),
+        DhDateTime.Parse("2021-04-05T16:00:00-0500"),
+        DhDateTime.Parse("2021-04-05T16:30:00-0500")
+      };
+      var priceData = new[] { 2.5, 3.7, 3.0, 100.50, 110 };
+      var sizeData = new[] { 52, 14, 73, 11, 6 };
+      var bidData = new double?[] { null, 3.4, 3.4, 97, 102 };
+      var bidSizeData = new int?[] { null, 20, 20, 5, 13 };
+      var askData = new double?[] { null, 3.4, 3.4, 105, 110 };
+      var askSizeData = new int?[] { null, 33, 33, 47, 15 };
+
+      using var tableMaker = new TableMaker();
+      tableMaker.AddColumn("Ticker", tickerData);
+      tableMaker.AddColumn("Timestamp", timestampData);
+      tableMaker.AddColumn("Price", priceData);
+      tableMaker.AddColumn("Size", sizeData);
+      tableMaker.AddColumn("Bid", bidData);
+      tableMaker.AddColumn("BidSize", bidSizeData);
+      tableMaker.AddColumn("Ask", askData);
+      tableMaker.AddColumn("AskSize", askSizeData);
+
+      CompareTable(
+        result,
+        "Ticker", ticker_data,
+        "Timestamp", timestamp_data,
+        "Price", priceData,
+        "Size", sizeData,
+        "Bid", bidData,
+        "BidSize", bid_size_data,
+        "Ask", askData,
+        "AskSize", askSizeData);
+    }
   }
 }
 /*
@@ -48,31 +141,6 @@ using deephaven::client::utility::TableMaker;
 using deephaven::dhcore::DateTime;
 
 namespace deephaven::client::tests {
-  TEST_CASE("Join", "[join]") {
-    auto tm = TableMakerForTests::Create();
-    auto table = tm.Table();
-
-    table = table.Where("ImportDate == `2017-11-01`");
-    auto last_close = table.LastBy("Ticker");
-    auto avg_view = table.View("Ticker", "Volume").AvgBy("Ticker");
-
-    auto joined = last_close.NaturalJoin(avg_view,
-        { "Ticker"},
-      { "ADV = Volume"});
-
-    auto filtered = joined.Select("Ticker", "Close", "ADV");
-
-    std::vector < std::string> ticker_data = { "XRX", "XYZZY", "IBM", "GME", "AAPL", "ZNGA"};
-    std::vector<double> close_data = { 53.8, 88.5, 38.7, 453, 26.7, 544.9 };
-    std::vector<double> adv_data = { 216000, 6060842, 138000, 138000000, 123000, 47211.50 };
-
-    CompareTable(
-        filtered,
-        "Ticker", ticker_data,
-        "Close", close_data,
-        "ADV", adv_data
-    );
-  }
 
   TEST_CASE("Aj", "[join]") {
     auto tm = TableMakerForTests::Create();
