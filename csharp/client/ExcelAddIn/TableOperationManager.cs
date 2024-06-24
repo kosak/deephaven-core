5a2b744415d11a351b7b1473f5aef9c354c01da2
Corey Kosak <kosak@kosak.com>
2024-06-23 22:03:06 -0400

    tpnn

--- csharp/client/ExcelAddIn/TableOperationManager.cs
+++ csharp/client/ExcelAddIn/TableOperationManager.cs
@@ -2,11 +2,49 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Deephaven.DeephavenClient.ExcelAddIn;
 
 internal sealed class TableOperationManager {
+  private readonly object _sync = new();
+  private readonly HashSet<IDeephavenTableOperation> _tableOperations = new();
 
+  public void Register(IDeephavenTableOperation tableOperation, IStatusObserver statusObserver) {
+    Invoke(() => {
+      _tableOperations.Add(tableOperation);
+      tableOperation.Start(_clientOrStatus, statusObserver);
+    });
+  }
+
+  public void Unregister(IDeephavenTableOperation tableOperation) {
+    Invoke(() => {
+      _tableOperations.Remove(tableOperation);
+      tableOperation.Stop();
+    });
+  }
+
+  public void Connect(string connectionString) {
+    Invoke(() => ConnectHelper(connectionString));
+  }
+
+  private void ConnectHelper(string connectionString) {
+    try {
+      var newClient = DeephavenClient.Client.Connect(connectionString, new ClientOptions());
+      _clientOrStatus = ClientOrStatus.Of(newClient);
+    } catch (Exception ex) {
+      _clientOrStatus = ClientOrStatus.Of(ex.Message);
+    }
+
+    foreach (var top in _tableOperations) {
+      // try-catch
+      top.Stop();
+    }
+    foreach (var top in _tableOperations) {
+      // try-catch
+      top.Start(_clientOrStatus, statusObserverDoNotWant);
+    }
+  }
 }
