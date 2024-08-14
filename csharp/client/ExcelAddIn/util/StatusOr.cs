6a1294b8f8d5e960bbc0c4da7ffa08f02b9a77d6
Corey Kosak <kosak@kosak.com>
2024-08-14 16:30:37 -0400

    tpnn

--- csharp/client/ExcelAddIn/util/StatusOr.cs
+++ csharp/client/ExcelAddIn/util/StatusOr.cs
@@ -3,19 +3,27 @@
 namespace Deephaven.ExcelAddIn.Util;
 
 public class StatusOr<T> {
-  public readonly string? Status;
-  public readonly T? Value;
+  private readonly string? _status;
+  private readonly T? _value;
 
   public static StatusOr<T> OfStatus(string status) {
-
+    return new StatusOr<T>(status, default);
   }
 
   public static StatusOr<T> OfValue(T value) {
+    return new StatusOr<T>(null, value);
+  }
 
+  public StatusOr(string? status, T? value) {
+    _status = status;
+    _value = value;
   }
 
   public bool TryGetValue(
     [NotNullWhen(true)]out T? value,
     [NotNullWhen(false)]out string? status) {
+    status = _status;
+    value = _value;
+    return value != null;
   }
 }
