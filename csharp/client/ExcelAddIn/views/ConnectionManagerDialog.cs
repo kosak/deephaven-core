607cbd22ff7bb02b423a0f7c6aeaa91d4351c036
Corey Kosak <kosak@kosak.com>
2024-08-20 18:44:35 -0400

    oh boy now I have a reconnect button

--- csharp/client/ExcelAddIn/views/ConnectionManagerDialog.cs
+++ csharp/client/ExcelAddIn/views/ConnectionManagerDialog.cs
@@ -1,37 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-namespace Deephaven.ExcelAddIn.Views;
+﻿namespace Deephaven.ExcelAddIn.Views;
 
 public partial class ConnectionManagerDialog : Form {
-  private const string ClickableColumnName = "clickable_column_button";
+  private const string SettingsButtonColumnName = "settings_button_column";
+  private const string ReconnectButtonColumnName = "reconnect_button_column";
   private readonly BindingSource _bindingSource = new();
-  private readonly List<Action> _clickActions = new();
+  private readonly List<Action> _settingsActions = new();
+  private readonly List<Action> _reconnectActions = new();
 
   public ConnectionManagerDialog() {
     InitializeComponent();
 
     _bindingSource.DataSource = typeof(HyperZamboniRow);
     dataGridView1.DataSource = _bindingSource;
-    var buttonColumn = new DataGridViewButtonColumn();
-    buttonColumn.Name = ClickableColumnName;
-    buttonColumn.HeaderText = "Settings";
-    buttonColumn.Text = "Change";
-    buttonColumn.UseColumnTextForButtonValue = true;
-    dataGridView1.Columns.Add(buttonColumn);
+    var settingsButtonColumn = new DataGridViewButtonColumn {
+      Name = SettingsButtonColumnName,
+      HeaderText = "Settings",
+      Text = "Change",
+      UseColumnTextForButtonValue = true
+    };
+    var reconnectButtonColumn = new DataGridViewButtonColumn {
+      Name = ReconnectButtonColumnName,
+      HeaderText = "Reconnect",
+      Text = "Reconnect",
+      UseColumnTextForButtonValue = true
+    };
+
+
+    dataGridView1.Columns.Add(settingsButtonColumn);
+    dataGridView1.Columns.Add(reconnectButtonColumn);
+
     dataGridView1.CellClick += DataGridView1_CellClick;
   }
 
-  public void AddRow(HyperZamboniRow row, Action onClick) {
+  public void AddRow(HyperZamboniRow row, Action settingsAction, Action reconnectAction) {
     _bindingSource.Add(row);
-    _clickActions.Add(onClick);
+    _settingsActions.Add(settingsAction);
+    _reconnectActions.Add(reconnectAction);
   }
 
   private void DataGridView1_CellClick(object? sender, DataGridViewCellEventArgs e) {
-    if (e.RowIndex < 0 || dataGridView1.Columns[e.ColumnIndex].Name != ClickableColumnName) {
+    if (e.RowIndex < 0) {
       return;
     }
 
-    _clickActions[e.RowIndex]();
+    var name = dataGridView1.Columns[e.ColumnIndex].Name;
+    if (name == SettingsButtonColumnName) {
+      _settingsActions[e.RowIndex]();
+    } else if (name == ReconnectButtonColumnName) {
+      _reconnectActions[e.RowIndex]();
+    }
   }
 }
