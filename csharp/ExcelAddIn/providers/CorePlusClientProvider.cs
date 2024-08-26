b28f5d2a4baa9b01feb7f189f31fbad590d0b4ef
Corey Kosak <kosak@kosak.com>
2024-08-26 17:25:19 -0400

    ok I dunno

--- csharp/ExcelAddIn/providers/CorePlusClientProvider.cs
+++ csharp/ExcelAddIn/providers/CorePlusClientProvider.cs
@@ -42,7 +42,7 @@ internal class CorePlusClientProvider : IObservable<StatusOr<Client>>, IDisposab
       observer.OnNext(_client);
     });
 
-    return new ActionAsDisposable(() => {
+    return ActionAsDisposable.Create(() => {
       _workerThread.Invoke(() => {
         _observers.Remove(observer, out _);
       });
