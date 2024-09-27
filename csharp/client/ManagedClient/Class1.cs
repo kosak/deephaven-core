1cccc14df892ac98e5b405431d7aa5a4c6b520d1
Corey Kosak <kosak@kosak.com>
2024-09-27 19:38:44 -0400

    tobb

--- csharp/client/ManagedClient/Class1.cs
+++ csharp/client/ManagedClient/Class1.cs
@@ -1,10 +1,21 @@
-﻿using Io.Deephaven.Proto.Backplane.Grpc;
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using Io.Deephaven.Proto.Backplane.Grpc;
 
 namespace Deephaven.ManagedClient;
 
 public class Class1 {
-  public static void main(String[] args) {
-    Ticket t;
+  public static void Zamboni() {
+    using var channel = GrpcChannel.ForAddress("http://10.0.4.109:10000");
+    var cfs = new ConfigService.ConfigServiceClient(channel);
+
+    var headers = new Metadata();
+    headers.Add("authorization", "Anonymous");
+
+    var req = new ConfigurationConstantsRequest();
+    var resp = cfs.GetConfigurationConstants(req, headers);
+
+    Console.WriteLine(resp);
   }
 
 
---
+++ csharp/client/RunManagerdClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using Deephaven.ManagedClient;
+
+namespace Deephaven.RunManangedClient;
+
+public static class Program {
+  public static void Main(string[] args) {
+    try {
+      Class1.Zamboni();
+    } catch (Exception ex) {
+      Console.WriteLine(ex.Message);
+    }
+  }
+
+}
