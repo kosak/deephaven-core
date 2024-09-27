9cb2cb80c430f8947899fb0bc59bc97915be3b6f
Corey Kosak <kosak@kosak.com>
2024-09-27 18:51:15 -0400

    omg

--- csharp/client/ManagedClient/Class1.cs
+++ csharp/client/ManagedClient/Class1.cs
@@ -1,5 +1,11 @@
-﻿namespace Deephaven.ManagedClient;
+﻿using Io.Deephaven.Proto.Backplane.Grpc;
+
+namespace Deephaven.ManagedClient;
 
 public class Class1 {
+  public static void main(String[] args) {
+    Ticket t;
+  }
+
 
 }
