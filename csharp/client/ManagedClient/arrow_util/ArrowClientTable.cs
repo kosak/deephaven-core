a8608f474d59cd2dd24fe53091cf2b9cd03a6d5d
Corey Kosak <kosak@kosak.com>
2024-09-28 10:42:38 -0400

    blah

--- csharp/client/ManagedClient/arrow_util/ArrowClientTable.cs
+++ csharp/client/ManagedClient/arrow_util/ArrowClientTable.cs
@@ -1,62 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Apache.Arrow;
 
 namespace Deephaven.ManagedClient;
 
 public class ArrowClientTable : ClientTable {
-  struct Private { };
-  using ClientTable = deephaven::dhcore::clienttable::ClientTable;
-  using SchemaType = deephaven::dhcore::clienttable::Schema;
-  public:
+  static ClientTable Create(Apache.Arrow.Table arrowTable) {
+    var schema = ArrowUtil.MakeDeephavenSchema(arrowTable.Schema);
+    var rowSequence = RowSequence.CreateSequential(0, arrowTable.RowCount);
 
-  static std::shared_ptr<ClientTable> Create(std::shared_ptr<arrow::Table> arrow_table) {
-    auto schema = ArrowUtil::MakeDeephavenSchema(*arrow_table->schema());
-    auto row_sequence = RowSequence::CreateSequential(0, arrow_table->num_rows());
-
-    auto column_sources = MakeReservedVector<std::shared_ptr<ColumnSource>>(arrow_table->num_columns());
-    for (const auto &chunked_array : arrow_table->columns()) {
-      column_sources.push_back(MakeColumnSource(*chunked_array));
+    var columnSources = new List<ColumnSource>();
+    for (var i = 0; i != arrowTable.ColumnCount; ++i) {
+      var col = arrowTable.Column(i);
+      columnSources.Add(MakeColumnSource(col));
     }
 
-    return std::make_shared<ArrowClientTable>(Private(), std::move(arrow_table),
-      std::move(schema), std::move(row_sequence), std::move(column_sources));
-}
-
-  ArrowClientTable(Private, std::shared_ptr<arrow::Table> arrow_table,
-    std::shared_ptr<SchemaType> schema, std::shared_ptr<RowSequence> row_sequence,
-    std::vector<std::shared_ptr<ColumnSource>> column_sources);
-  ArrowClientTable(ArrowClientTable &&other) noexcept;
-  ArrowClientTable &operator=(ArrowClientTable &&other) noexcept;
-  ~ArrowClientTable() final;
-
-  [[nodiscard]]
-  std::shared_ptr<RowSequence> GetRowSequence() const final {
-    return row_sequence_;
+    return new ArrowClientTable(arrowTable, schema, rowSequence, columnSources.ToArray());
   }
 
-  std::shared_ptr<ColumnSource> GetColumn(size_t column_index) const final;
+  private readonly Apache.Arrow.Table _arrowTable;
+  public Schema Schema { get; }
+  public RowSequence RowSequence { get; }
+  private readonly ColumnSource[] _columnSources;
 
-  [[nodiscard]]
-  size_t NumRows() const final {
-    return arrow_table_->num_rows();
+  private ArrowClientTable(Apache.Arrow.Table arrowTable, Schema schema, RowSequence rowSequence,
+    ColumnSource[] columnSources) {
+    _arrowTable = arrowTable;
+    Schema = schema;
+    RowSequence = rowSequence;
+    _columnSources = columnSources;
   }
 
-  [[nodiscard]]
-  size_t NumColumns() const final {
-    return arrow_table_->num_columns();
-  }
+  ColumnSource GetColumn(int columnIndex) => _columnSources[columnIndex];
 
-  [[nodiscard]]
-  std::shared_ptr<SchemaType> Schema() const final {
-    return schema_;
-  }
-
-  private:
-  std::shared_ptr<arrow::Table> arrow_table_;
-  std::shared_ptr<SchemaType> schema_;
-  std::shared_ptr<RowSequence> row_sequence_;
-  std::vector<std::shared_ptr<ColumnSource>> column_sources_;
+  public Int64 NumRows => _arrowTable.RowCount;
+  public Int64 NumColumns => _arrowTable.ColumnCount;
 }
