7a6976035674abfe64b1ae223b404ae9204b63a5
Corey Kosak <kosak@kosak.com>
2024-09-30 23:31:53 -0400

    tpon

--- csharp/client/ManagedClient/arrow_util/ArrowColumnSource.cs
+++ csharp/client/ManagedClient/arrow_util/ArrowColumnSource.cs
@@ -2,136 +2,92 @@
 
 namespace Deephaven.ManagedClient;
 
-public class CharArrowColumnSource(ChunkedArray chunkedArray) : ICharColumnSource {
+public class ArrowColumnSource<T>(ChunkedArray chunkedArray) : IColumnSource<T> {
   public void FillChunk(RowSequence rows, Chunk destData, Chunk<bool>? nullFlags) {
+    var visitor = new FillChunkVisitor(chunkedArray, rows, destData, nullFlags);
+    Accept(visitor);
+  }
+
+  public void Accept(IColumnSourceVisitor visitor) {
+    IColumnSource.Accept(this, visitor);
+  }
+}
+
+class FillChunkVisitor(ChunkedArray chunkedArray, RowSequence rows, Chunk destData, Chunk<bool>? nullFlags)
+  : IColumnSourceVisitor<ICharColumnSource>,
+    IColumnSourceVisitor<IByteColumnSource>,
+    IColumnSourceVisitor<IInt16ColumnSource>,
+    IColumnSourceVisitor<IInt32ColumnSource>,
+    IColumnSourceVisitor<IInt64ColumnSource>,
+    IColumnSourceVisitor<IFloatColumnSource>,
+    IColumnSourceVisitor<IDoubleColumnSource>,
+    IColumnSourceVisitor<IBooleanColumnSource>,
+    IColumnSourceVisitor<IStringColumnSource>,
+    IColumnSourceVisitor<ITimestampColumnSource>,
+    IColumnSourceVisitor<ILocalDateColumnSource>,
+    IColumnSourceVisitor<ILocalTimeColumnSource> {
+  public void Visit(ICharColumnSource src) {
     var tc = new TransformingCopier<UInt16, char>((CharChunk)destData, nullFlags, v => (char)v);
     tc.FillChunk(rows, chunkedArray);
   }
 
-  public void AcceptVisitor(IColumnSourceVisitor visitor) {
-    visitor.Visit(this);
-  }
-}
-
-public class ByteArrowColumnSource(ChunkedArray chunkedArray) : IByteColumnSource {
-  public void FillChunk(RowSequence rows, Chunk destData, Chunk<bool>? nullFlags) {
+  public void Visit(IByteColumnSource src) {
     var vc = new ValueCopier<sbyte>((ByteChunk)destData, nullFlags);
     vc.FillChunk(rows, chunkedArray);
   }
 
-  public void AcceptVisitor(IColumnSourceVisitor visitor) {
-    visitor.Visit(this);
-  }
-}
-
-public class Int16ArrowColumnSource(ChunkedArray chunkedArray) : IInt16ColumnSource {
-  public void FillChunk(RowSequence rows, Chunk destData, BooleanChunk? nullFlags) {
+  public void Visit(IInt16ColumnSource src) {
     var vc = new ValueCopier<Int16>((Int16Chunk)destData, nullFlags);
     vc.FillChunk(rows, chunkedArray);
   }
 
-  public void AcceptVisitor(IColumnSourceVisitor visitor) {
-    visitor.Visit(this);
-  }
-}
-
-public class Int32ArrowColumnSource(ChunkedArray chunkedArray) : IInt32ColumnSource {
-  public void FillChunk(RowSequence rows, Chunk destData, BooleanChunk? nullFlags) {
+  public void Visit(IInt32ColumnSource src) {
     var vc = new ValueCopier<Int32>((Int32Chunk)destData, nullFlags);
     vc.FillChunk(rows, chunkedArray);
   }
 
-  public void AcceptVisitor(IColumnSourceVisitor visitor) {
-    visitor.Visit(this);
-  }
-}
-
-public class Int64ArrowColumnSource(ChunkedArray chunkedArray) : IInt64ColumnSource {
-  public void FillChunk(RowSequence rows, Chunk destData, BooleanChunk? nullFlags) {
+  public void Visit(IInt64ColumnSource src) {
     var vc = new ValueCopier<Int64>((Int64Chunk)destData, nullFlags);
     vc.FillChunk(rows, chunkedArray);
   }
 
-  public void AcceptVisitor(IColumnSourceVisitor visitor) {
-    visitor.Visit(this);
-  }
-}
-
-public class FloatArrowColumnSource(ChunkedArray chunkedArray) : IFloatColumnSource {
-  public void FillChunk(RowSequence rows, Chunk destData, BooleanChunk? nullFlags) {
+  public void Visit(IFloatColumnSource src) {
     var vc = new ValueCopier<float>((FloatChunk)destData, nullFlags);
     vc.FillChunk(rows, chunkedArray);
   }
 
-  public void AcceptVisitor(IColumnSourceVisitor visitor) {
-    visitor.Visit(this);
-  }
-}
-
-public class DoubleArrowColumnSource(ChunkedArray chunkedArray) : IDoubleColumnSource {
-  public void FillChunk(RowSequence rows, Chunk destData, BooleanChunk? nullFlags) {
+  public void Visit(IDoubleColumnSource src) {
     var vc = new ValueCopier<double>((DoubleChunk)destData, nullFlags);
     vc.FillChunk(rows, chunkedArray);
   }
 
-  public void AcceptVisitor(IColumnSourceVisitor visitor) {
-    visitor.Visit(this);
-  }
-}
-
-public class BooleanArrowColumnSource(ChunkedArray chunkedArray) : IBooleanColumnSource {
-  public void FillChunk(RowSequence rows, Chunk destData, BooleanChunk? nullFlags) {
+  public void Visit(IBooleanColumnSource src) {
     var vc = new ValueCopier<bool>((BooleanChunk)destData, nullFlags);
     vc.FillChunk(rows, chunkedArray);
   }
 
-  public void AcceptVisitor(IColumnSourceVisitor visitor) {
-    visitor.Visit(this);
-  }
-}
-
-public class StringArrowColumnSource(ChunkedArray chunkedArray) : IStringColumnSource {
-  public void FillChunk(RowSequence rows, Chunk destData, BooleanChunk? nullFlags) {
-    var rc = new ReferenceCopier<string>((StringChunk)destData, nullFlags);
-    rc.FillChunk(rows, chunkedArray);
+  public void Visit(IStringColumnSource src) {
+    var vc = new ReferenceCopier<string>((StringChunk)destData, nullFlags);
+    vc.FillChunk(rows, chunkedArray);
   }
 
-  public void AcceptVisitor(IColumnSourceVisitor visitor) {
-    visitor.Visit(this);
-  }
-}
-
-
-public class TimestampArrowColumnSource(ChunkedArray chunkedArray) : ITimestampColumnSource {
-  public void FillChunk(RowSequence rows, Chunk destData, BooleanChunk? nullFlags) {
+  public void Visit(ITimestampColumnSource src) {
     var tc = new TransformingCopier<Int64, DhDateTime>((DhDateTimeChunk)destData, nullFlags, DhDateTime.FromNanos);
     tc.FillChunk(rows, chunkedArray);
   }
 
-  public void AcceptVisitor(IColumnSourceVisitor visitor) {
-    visitor.Visit(this);
-  }
-}
-
-public class LocalDateArrowColumnSource(ChunkedArray chunkedArray) : ILocalDateColumnSource {
-  public void FillChunk(RowSequence rows, Chunk destData, BooleanChunk? nullFlags) {
+  public void Visit(ILocalDateColumnSource src) {
     var tc = new TransformingCopier<Int64, LocalDate>((LocalDateChunk)destData, nullFlags, LocalDate.FromMillis);
     tc.FillChunk(rows, chunkedArray);
   }
 
-  public void AcceptVisitor(IColumnSourceVisitor visitor) {
-    visitor.Visit(this);
-  }
-}
-
-public class LocalTimeArrowColumnSource(ChunkedArray chunkedArray) : ILocalTimeColumnSource {
-  public void FillChunk(RowSequence rows, Chunk destData, BooleanChunk? nullFlags) {
+  public void Visit(ILocalTimeColumnSource src) {
     var tc = new TransformingCopier<Int64, LocalTime>((LocalTimeChunk)destData, nullFlags, LocalTime.FromNanos);
     tc.FillChunk(rows, chunkedArray);
   }
 
-  public void AcceptVisitor(IColumnSourceVisitor visitor) {
-    visitor.Visit(this);
+  public void Visit(IColumnSource cs) {
+    throw new Exception($"IColumnSource type {cs.GetType().Name} not implemented");
   }
 }
 
