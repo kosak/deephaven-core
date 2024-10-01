e85a96909d9ea8f395cd93a3b66eb8a12c1d9d4b
Corey Kosak <kosak@kosak.com>
2024-09-30 21:17:43 -0400

    bbz

--- csharp/client/ManagedClient/column/ArrayColumnSource.cs
+++ csharp/client/ManagedClient/column/ArrayColumnSource.cs
@@ -1,7 +1,22 @@
-﻿
-namespace Deephaven.ManagedClient;
+﻿namespace Deephaven.ManagedClient;
 
-class GenericArrayColumnSource<T> : IColumnSource<T> {
+public abstract class ArrayColumnSource : IColumnSource {
+
 }
 
+public class ArrayColumnSource<T> : ArrayColumnSource, IColumnSource<T> {
+  private readonly T[] _data;
+  private readonly bool[] _nulls;
 
+  public void FillChunk(RowSequence rows, Chunk dest, BooleanChunk? nullFlags) {
+    foreach (var interval in rows.Intervals) {
+
+    }
+    throw new NotImplementedException();
+  }
+
+  public void Accept(IColumnSourceVisitor visitor) {
+
+    visitor.Visit(this);
+  }
+}
--- csharp/client/ManagedClient/column/ColumnSource.cs
+++ csharp/client/ManagedClient/column/ColumnSource.cs
@@ -1,38 +1,31 @@
-﻿global using ICharColumnSource = Deephaven.ManagedClient.IGenericColumnSource<char>;
-global using IByteColumnSource = Deephaven.ManagedClient.IGenericColumnSource<sbyte>;
-global using IInt16ColumnSource = Deephaven.ManagedClient.IGenericColumnSource<System.Int16>;
-global using IInt32ColumnSource = Deephaven.ManagedClient.IGenericColumnSource<System.Int32>;
-global using IInt64ColumnSource = Deephaven.ManagedClient.IGenericColumnSource<System.Int64>;
-global using IFloatColumnSource = Deephaven.ManagedClient.IGenericColumnSource<float>;
-global using IDoubleColumnSource = Deephaven.ManagedClient.IGenericColumnSource<double>;
-global using IBooleanColumnSource = Deephaven.ManagedClient.IGenericColumnSource<bool>;
-global using IStringColumnSource = Deephaven.ManagedClient.IGenericColumnSource<string>;
-global using ITimestampColumnSource = Deephaven.ManagedClient.IGenericColumnSource<Deephaven.ManagedClient.DhDateTime>;
-global using ILocalDateColumnSource = Deephaven.ManagedClient.IGenericColumnSource<Deephaven.ManagedClient.LocalDate>;
-global using ILocalTimeColumnSource = Deephaven.ManagedClient.IGenericColumnSource<Deephaven.ManagedClient.LocalTime>;
+﻿global using ICharColumnSource = Deephaven.ManagedClient.IColumnSource<char>;
+global using IByteColumnSource = Deephaven.ManagedClient.IColumnSource<sbyte>;
+global using IInt16ColumnSource = Deephaven.ManagedClient.IColumnSource<System.Int16>;
+global using IInt32ColumnSource = Deephaven.ManagedClient.IColumnSource<System.Int32>;
+global using IInt64ColumnSource = Deephaven.ManagedClient.IColumnSource<System.Int64>;
+global using IFloatColumnSource = Deephaven.ManagedClient.IColumnSource<float>;
+global using IDoubleColumnSource = Deephaven.ManagedClient.IColumnSource<double>;
+global using IBooleanColumnSource = Deephaven.ManagedClient.IColumnSource<bool>;
+global using IStringColumnSource = Deephaven.ManagedClient.IColumnSource<string>;
+global using ITimestampColumnSource = Deephaven.ManagedClient.IColumnSource<Deephaven.ManagedClient.DhDateTime>;
+global using ILocalDateColumnSource = Deephaven.ManagedClient.IColumnSource<Deephaven.ManagedClient.LocalDate>;
+global using ILocalTimeColumnSource = Deephaven.ManagedClient.IColumnSource<Deephaven.ManagedClient.LocalTime>;
 
 namespace Deephaven.ManagedClient;
 
 public interface IColumnSource {
   void FillChunk(RowSequence rows, Chunk dest, BooleanChunk? nullFlags);
-  void AcceptVisitor(IColumnSourceVisitor visitor);
+  void Accept(IColumnSourceVisitor visitor);
 }
 
-public interface IGenericColumnSource<T> : IColumnSource {
+public interface IColumnSource<T> : IColumnSource {
 
 }
 
 public interface IColumnSourceVisitor {
-  void Visit(ICharColumnSource cs);
-  void Visit(IByteColumnSource cs);
-  void Visit(IInt16ColumnSource cs);
-  void Visit(IInt32ColumnSource cs);
-  void Visit(IInt64ColumnSource cs);
-  void Visit(IFloatColumnSource cs);
-  void Visit(IDoubleColumnSource cs);
-  void Visit(IBooleanColumnSource cs);
-  void Visit(IStringColumnSource cs);
-  void Visit(ITimestampColumnSource cs);
-  void Visit(ILocalDateColumnSource cs);
-  void Visit(ILocalTimeColumnSource cs);
+  void Visit(IColumnSource cs);
+}
+
+public interface IColumnSourceVisitor<in T> where T : IColumnSourceVisitor {
+  void Visit(T cs);
 }
