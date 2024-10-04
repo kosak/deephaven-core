ce01b737fe38d478757a9fe2ead02ed83925c2cb
Corey Kosak <kosak@kosak.com>
2024-10-04 01:32:12 -0400

    blah

--- csharp/client/DndClient/utility/Utility.cs
+++ csharp/client/DndClient/utility/Utility.cs
@@ -4,33 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Channels;
 using System.Threading.Tasks;
+using Deephaven.ManagedClient;
+using Grpc.Net.Client;
 
 namespace DeephavenEnterprise.DndClient;
 
 public static class Utility {
-  public static Grpc.Net.Channel CreateChannel(
-  const std::string &who,
-  const std::string &target,
-  const utility::ClientOptions &options) {
-    if (!options.UseTls() && !options.TlsRootCerts().empty()) {
-      const std::string message =
-        who + ": ClientOptions: "
-      "UseTls is false but pem provided";
-      throw std::runtime_error(DEEPHAVEN_LOCATION_STR(message));
+  public static GrpcChannel CreateChannel(string who, string target, ClientOptions? options) {
+    options ??= new ClientOptions();
+    if (!options.UseTls && !options.TlsRootCerts.IsEmpty()) {
+      throw new Exception("UseTls is false but pem provided");
     }
 
+    // grpc::ChannelArguments channel_args;
+    // for (const auto &opt : options.IntOptions()) {
+    //   channel_args.SetInt(opt.first, opt.second);
+    // }
+    // for (const auto &opt : options.StringOptions()) {
+    //   channel_args.SetString(opt.first, opt.second);
+    // }
+    //
 
-    grpc::ChannelArguments channel_args;
-    for (const auto &opt : options.IntOptions()) {
-      channel_args.SetInt(opt.first, opt.second);
-    }
-    for (const auto &opt : options.StringOptions()) {
-      channel_args.SetString(opt.first, opt.second);
-    }
+    var channelOptions = new GrpcChannelOptions {
+
+    };
 
 
     const auto credentials = GetCredentials(options.UseTls(), options.TlsRootCerts(),
       options.ClientCertChain(), options.ClientPrivateKey());
+
+    var channel = new GrpcChannel(address, channelOptions);
+    
     auto channel = grpc::CreateCustomChannel(
       target,
       credentials,
