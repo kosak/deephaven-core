7120868c01d373d316169255d0ff7810011e5bff
Corey Kosak <kosak@kosak.com>
2024-10-19 17:19:18 -0400

    blah reorg

--- csharp/ExcelAddInInstaller/CustomActions/CustomActions.cs
+++ csharp/ExcelAddInInstaller/CustomActions/CustomActions.cs
@@ -1,49 +1,88 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.Win32;
 
-namespace CustomActions
-{
-	public class CustomActions
-	{
-		public static int CustomAction1(string aMsiHandle)
-		{
-			MsiSession session = new MsiSession(aMsiHandle);
+namespace Deephaven.ExcelAddInInstaller.CustomActions {
+  public class OpenEntry {
+    public String Key { get; }
+    public String Value { get; }
+  }
 
-			// This will be used to show the message box or any dialog modal to installer window
-			Win32Window msiWindow = new Win32Window(session.GetMsiWindowHandle());
 
-			System.Windows.Forms.MessageBox.Show(msiWindow, "Attach your debugger now!", "Advanced Installer custom action");
+    public static bool TryDetermineBitness(out bool is64Bit, out string failureReason) {
+      is64Bit = false;
+      failureReason = "";
+      var regView = Environment.Is64BitOperatingSystem ? RegistryView.Registry64 : RegistryView.Registry32;
+      var regBase = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, regView);
+      var subKey = regBase.OpenSubKey(Bitness.Key, false);
+      if (subKey == null) {
+        failureReason = $"Couldn't find registry key {Bitness.Key}";
+        return false;
+      }
 
-			// Log data passed as action data
-			string infoMessage = string.Format("User data: \"{0}\"", session.CustomActionData);
-			session.Log(infoMessage, MsiSession.InstallMessage.INFO);
+      var bitnessValue = subKey.GetValue(Bitness.Name);
+      if (bitnessValue == null) {
+        failureReason = $"Couldn't find entry for {Bitness.Name}";
+        return false;
+      }
 
-			// Get property value
-			string myProperty = "MY_PROPERTY";
-			string myPropertyValue = session.GetProperty(myProperty);
+      if (bitnessValue.Equals(Bitness.Value64)) {
+        is64Bit = true;
+        return true;
+      }
 
-			// Log property MY_PROPERTY value
-			infoMessage = string.Format("Property \"{0}\" has value: \"{1}\"", myProperty, myPropertyValue);
-			session.Log(infoMessage, MsiSession.InstallMessage.INFO);
+      if (bitnessValue.Equals(Bitness.Value32)) {
+        is64Bit = false;
+        return true;
+      }
 
-			string mySecondProperty = "MY_SECOND_PROPERTY";
-			string mySecondPropertyValue = string.IsNullOrEmpty(myPropertyValue)
-																	 ? "Advanced"
-																	 : "Installer";
+      failureReason = $"Unexpected bitness value {bitnessValue}";
+      return false;
+    }
 
-			// Set property value
-			session.SetProperty(mySecondProperty, mySecondPropertyValue);
 
-			// Log property MY_SECOND_PROPERTY value update
-			infoMessage = string.Format("Property \"{0}\" was set as \"{1}\"", mySecondProperty, mySecondPropertyValue);
-			session.Log(infoMessage, MsiSession.InstallMessage.INFO);
 
-			return 0;
-		}
-	}
+    public static int CustomAction1(string aMsiHandle) {
+      MsiSession session = new MsiSession(aMsiHandle);
 
-	public class Win32Window : System.Windows.Forms.IWin32Window
-	{
-		public IntPtr Handle { get; private set; }
-		public Win32Window(IntPtr aWindowHandle) { Handle = aWindowHandle; }
-	}
+      // This will be used to show the message box or any dialog modal to installer window
+      Win32Window msiWindow = new Win32Window(session.GetMsiWindowHandle());
+
+      System.Windows.Forms.MessageBox.Show(msiWindow, "Attach your debugger now!", "Advanced Installer custom action");
+
+      // Log data passed as action data
+      string infoMessage = string.Format("User data: \"{0}\"", session.CustomActionData);
+      session.Log(infoMessage, MsiSession.InstallMessage.INFO);
+
+      // Get property value
+      string myProperty = "MY_PROPERTY";
+      string myPropertyValue = session.GetProperty(myProperty);
+
+      // Log property MY_PROPERTY value
+      infoMessage = string.Format("Property \"{0}\" has value: \"{1}\"", myProperty, myPropertyValue);
+      session.Log(infoMessage, MsiSession.InstallMessage.INFO);
+
+      string mySecondProperty = "MY_SECOND_PROPERTY";
+      string mySecondPropertyValue = string.IsNullOrEmpty(myPropertyValue)
+        ? "Advanced"
+        : "Installer";
+
+      // Set property value
+      session.SetProperty(mySecondProperty, mySecondPropertyValue);
+
+      // Log property MY_SECOND_PROPERTY value update
+      infoMessage = string.Format("Property \"{0}\" was set as \"{1}\"", mySecondProperty, mySecondPropertyValue);
+      session.Log(infoMessage, MsiSession.InstallMessage.INFO);
+
+      return 0;
+    }
+  }
+
+  public class Win32Window : System.Windows.Forms.IWin32Window {
+    public IntPtr Handle { get; private set; }
+
+    public Win32Window(IntPtr aWindowHandle) {
+      Handle = aWindowHandle;
+    }
+  }
 }
---
+++ csharp/ExcelAddInInstaller/CustomActions/OpenEntryManager.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+
+namespace Deephaven.ExcelAddInInstaller.CustomActions {
+  public class OpenEntryManager {
+    public static bool GetOpenEntries(out List<OpenEntry> entries, out string failureReason) {
+      entries = null;
+      failureReason = "";
+      var subKey = Registry.CurrentUser.OpenSubKey(RegistryKeys.OpenEntries.Key, false);
+      if (subKey == null) {
+        failureReason = $"Couldn't find registry key {RegistryKeys.OpenEntries.Key}";
+        return false;
+      }
+
+      var valueNames = subKey.GetValueNames();
+      entries = new List<OpenEntry>();
+      foreach (var valueName in valueNames) {
+        var value = subKey.GetValue(valueName);
+        if (value == null) {
+          failureReason = $"Entry is null for value valueName";
+        }
+
+        if (!OpenEntry.TryParse(value, out var openEntry)) {
+          continue;
+        }
+
+        entries.Add(openEntry);
+      }
+
+      entries.Sort(oe => oe.Index);
+      return true;
+    }
+
+  }
+}
