5c7cfb20ddab4cb34b50112df2da99b1254e57b9
Corey Kosak <coreykosak@deephaven.io>
2025-01-05 19:19:15 -0500

    tpnn immer-pain

---
+++ sandbox/immer-pain/main.cpp
@@ -0,0 +1,86 @@
+#include "immer/map.hpp"
+#include <iostream>
+#include <map>
+#include <memory>
+
+class SubscriptionContainerImpl;
+class ConstIteratorImpl;
+
+class SubscriptionContainer {
+public:
+  class ConstIterator;
+
+  explicit SubscriptionContainer(std::shared_ptr<SubscriptionContainerImpl> impl) : impl_(std::move(impl)) {}
+
+  ConstIterator begin() const;
+  ConstIterator end() const;
+  ConstIterator find(int key);
+
+private:
+  std::shared_ptr<SubscriptionContainerImpl> impl_;
+};
+
+class SubscriptionContainer::ConstIterator {
+public:
+  const std::pair<int, const char*> &operator*();
+  const std::pair<int, const char*> *operator->();
+
+private:
+  std::shared_ptr<ConstIteratorImpl> impl_;
+
+  friend bool operator==(const ConstIterator &lhs, const ConstIterator &rhs);
+  friend bool operator!=(const ConstIterator &lhs, const ConstIterator &rhs);
+  friend ConstIterator &operator++(ConstIterator &);
+  friend ConstIterator operator++(ConstIterator&, int);
+};
+
+class SubscriptionContainerImpl {
+public:
+  explicit SubscriptionContainerImpl(immer::map<int, const char*> m) : m_(std::move(m)) {}
+private:
+  immer::map<int, const char*> m_;
+};
+
+std::map<int, const char*> MapMaker() {
+  immer::map<int, const char *> m;
+  auto m2 = m.set(0, "hello");
+  auto m3 = m2.set(3, "bye");
+
+  std::map<int, const char*> result;
+  result[0] = "hello";
+  result[3] = "goodbye";
+  return result;
+}
+
+SubscriptionContainer WrapperMaker() {
+  immer::map<int, const char *> m;
+  auto m2 = m.set(0, "hello");
+  auto m3 = m2.set(3, "bye");
+
+  auto impl = std::make_shared<SubscriptionContainerImpl>(std::move(m));
+  SubscriptionContainer sc(std::move(impl));
+  return sc;
+}
+
+int main() {
+  auto submap = WrapperMaker();
+  for (const auto &e : submap) {
+    std::cout << e.first << ", " << e.second << "\n";
+  }
+
+  auto j = submap.find(3);
+  auto k = submap.find(4);
+
+  auto jb = j != submap.end();
+  auto kb = k != submap.end();
+
+  std::cout << "jb is " << jb << ", " << "kb is " << kb << "\n";
+
+  if (jb) {
+    std::cout << j->first << ", " << j->second << "\n";
+
+    ++j;
+    auto jb2 = j != submap.end();
+    std::cout << "jb2 is " << jb2 << "\n";
+  }
+}
