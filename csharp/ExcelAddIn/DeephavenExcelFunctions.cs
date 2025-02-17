ba16796fb427b517531982add301abff608ad366
Corey Kosak <kosak@kosak.com>
2025-02-17 13:29:21 -0500

    repro - whose bug is this?

--- csharp/ExcelAddIn/DeephavenExcelFunctions.cs
+++ csharp/ExcelAddIn/DeephavenExcelFunctions.cs
@@ -10,63 +10,11 @@ namespace Deephaven.ExcelAddIn;
 public static class DeephavenExcelFunctions {
   private static readonly StateManager StateManager = new();
 
-  [ExcelCommand(MenuName = "Deephaven", MenuText = "Connections")]
-  public static void ShowConnectionsDialog() {
-    EndpointManagerDialogFactory.CreateAndShow(StateManager);
-  }
-
-  [ExcelCommand(MenuName = "Debug", MenuText = "kosak Core local")]
-  public static void AddKosakConnection() {
-    var id = new EndpointId("con1");
-    var config = EndpointConfigBase.OfCore(id, "10.0.4.109:10000", true);
-    StateManager.SetCredentials(config);
-  }
-
   [ExcelFunction(Description = "Snapshots a table", IsThreadSafe = true)]
   public static object DEEPHAVEN_SNAPSHOT(string tableDescriptor, object filter, object wantHeaders) {
-    if (!TryInterpretCommonArgs(tableDescriptor, filter, wantHeaders, out var tq, out var wh, out var errorText)) {
-      return errorText;
-    }
-
-    // These two are used by ExcelDNA to share results for identical invocations. The functionName is arbitary but unique.
-    const string functionName = "Deephaven.ExcelAddIn.DeephavenExcelFunctions.DEEPHAVEN_SNAPSHOT";
-    var parms = new[] { tableDescriptor, filter, wantHeaders };
-    ExcelObservableSource eos = () => new SnapshotOperation(tq, wh, StateManager);
-    return ExcelAsyncUtil.Observe(functionName, parms, eos);
-  }
-
-  [ExcelFunction(Description = "Subscribes to a table", IsThreadSafe = true)]
-  public static object DEEPHAVEN_SUBSCRIBE(string tableDescriptor, object filter, object wantHeaders) {
-    if (!TryInterpretCommonArgs(tableDescriptor, filter, wantHeaders, out var tq, out var wh, out string errorText)) {
-      return errorText;
-    }
-    // These two are used by ExcelDNA to share results for identical invocations. The functionName is arbitary but unique.
-    const string functionName = "Deephaven.ExcelAddIn.DeephavenExcelFunctions.DEEPHAVEN_SUBSCRIBE";
-    var parms = new[] { tableDescriptor, filter, wantHeaders };
-    ExcelObservableSource eos = () => new SubscribeOperation(tq, wh, StateManager);
-    return ExcelAsyncUtil.Observe(functionName, parms, eos);
-  }
-
-  private static bool TryInterpretCommonArgs(string tableDescriptor, object filter, object wantHeaders,
-    [NotNullWhen(true)]out TableQuad? tableQuadResult, out bool wantHeadersResult, out string errorText) {
-    tableQuadResult = null;
-    wantHeadersResult = false;
-    if (!TableTriple.TryParse(tableDescriptor, out var tt, out errorText)) {
-      return false;
-    }
-
-    if (!ExcelDnaHelpers.TryInterpretAs(filter, "", out var condition)) {
-      errorText = "Can't interpret FILTER argument";
-      return false;
-    }
-
-
-    if (!ExcelDnaHelpers.TryInterpretAs(wantHeaders, false, out wantHeadersResult)) {
-      errorText = "Can't interpret WANT_HEADERS argument";
-      return false;
-    }
-
-    tableQuadResult = new TableQuad(tt.EndpointId, tt.PersistentQueryId, tt.TableName, condition);
-    return true;
+    // if (!TryInterpretCommonArgs(tableDescriptor, filter, wantHeaders, out var tq, out var wh, out var errorText)) {
+    //   return errorText;
+    // }
+    throw new Exception("sad");
   }
 }
