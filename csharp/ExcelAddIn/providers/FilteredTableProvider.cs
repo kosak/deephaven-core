fb11a9390ceacf73ae663fe85e05926f38a57d43
Corey Kosak <kosak@kosak.com>
2025-02-18 20:48:29 -0500

    blkah

--- csharp/ExcelAddIn/providers/FilteredTableProvider.cs
+++ csharp/ExcelAddIn/providers/FilteredTableProvider.cs
@@ -6,41 +6,37 @@ using Deephaven.ManagedClient;
 namespace Deephaven.ExcelAddIn.Providers;
 
 internal class FilteredTableProvider :
-  IObserver<StatusOr<TableHandle>>,
-  // IObservable<StatusOr<TableHandle>>, // redundant, part of ITableProvider
-  ITableProvider {
+  IObserver<StatusOr<View<TableHandle>>>,
+  IObservable<StatusOr<View<TableHandle>>> {
 
   private readonly StateManager _stateManager;
-  private readonly WorkerThread _workerThread;
   private readonly EndpointId _endpointId;
   private readonly PersistentQueryId? _persistentQueryId;
   private readonly string _tableName;
   private readonly string _condition;
   private Action? _onDispose;
+  private readonly SequentialExecutor _executor = new();
   private IDisposable? _tableHandleSubscriptionDisposer = null;
   private readonly ObserverContainer<StatusOr<TableHandle>> _observers = new();
-  private StatusOr<TableHandle> _filteredTableHandle = StatusOr<TableHandle>.OfStatus("[No Filtered Table]");
+  private StatusOr<RefCounted<TableHandle>> _filteredTableHandle = StatusOr<RefCounted<TableHandle>>.OfStatus("[No Filtered Table]");
 
   public FilteredTableProvider(StateManager stateManager,
     EndpointId endpointId, PersistentQueryId? persistentQueryId, string tableName, string condition,
     Action onDispose) {
     _stateManager = stateManager;
-    _workerThread = stateManager.WorkerThread;
     _endpointId = endpointId;
     _persistentQueryId = persistentQueryId;
     _tableName = tableName;
     _condition = condition;
     _onDispose = onDispose;
-  }
 
-  public void Init() {
     // Subscribe to a condition-free table
     var tq = new TableQuad(_endpointId, _persistentQueryId, _tableName, "");
     Debug.WriteLine($"FTP is subscribing to TableHandle with {tq}");
     _tableHandleSubscriptionDisposer = _stateManager.SubscribeToTable(tq, this);
   }
 
-  public IDisposable Subscribe(IObserver<StatusOr<TableHandle>> observer) {
+  public IDisposable Subscribe(IObserver<StatusOr<View<TableHandle>>> observer) {
     _workerThread.EnqueueOrRun(() => {
       _observers.Add(observer, out _);
       observer.OnNext(_filteredTableHandle);
@@ -58,28 +54,33 @@ internal class FilteredTableProvider :
     });
   }
 
-  public void OnNext(StatusOr<TableHandle> tableHandle) {
-    // Get onto the worker thread if we're not already on it.
-    if (_workerThread.EnqueueOrNop(() => OnNext(tableHandle))) {
-      return;
-    }
+  public void OnNext(StatusOr<View<TableHandle>> tableHandleView) {
+    var tableHandle = tableHandleView.Share();
+    var versionCookie = new object();
+    _latestCookie.SetValue(versionCookie);
+    _executor.EnqueueIndependentWork(() => OnNextIndependentWork(versionCookie, tableHandle));
+  }
 
-    DisposeTableHandleState();
-
-    // If the new state is just a status message, make that our state and transmit to our observers
-    if (!tableHandle.GetValueOrStatus(out var th, out var status)) {
-      _observers.SetAndSendStatus(ref _filteredTableHandle, status);
-      return;
-    }
-
-    // It's a real TableHandle so start fetching the table. First notify our observers.
-    _observers.SetAndSendStatus(ref _filteredTableHandle, "Filtering");
-
+  private void OnNextIndependentWork(object versionCookie, StatusOr<RefCounted<TableHandle>> tableHandle) {
     try {
-      var filtered = th.Where(_condition);
-      _observers.SetAndSendValue(ref _filteredTableHandle, filtered);
-    } catch (Exception ex) {
-      _observers.SetAndSendStatus(ref _filteredTableHandle, ex.Message);
+      if (!tableHandle.GetValueOrStatus(out var th, out var status)) {
+        OnNextStoreResult(versionCookie, result);
+      } else {
+        // TODO: send two messages
+        OnNextStoreResult(versionCookie, result);
+        _observers.SetAndSendStatus(ref _filteredTableHandle, "Filtering");
+
+        try {
+          var filtered = th.Where(_condition);
+          _observers.SetAndSendValue(ref _filteredTableHandle, filtered);
+          OnNextStoreResult(versionCookie, result);
+        } catch (Exception ex) {
+          _observers.SetAndSendStatus(ref _filteredTableHandle, ex.Message);
+          OnNextStoreResult(versionCookie, result);
+        }
+      }
+    } finally {
+      tableHandle.Dispose();
     }
   }
 
---
+++ csharp/ExcelAddIn/util/SequentialExecutor.cs
@@ -0,0 +1,95 @@
+﻿using System.Diagnostics;
+using Deephaven.ManagedClient;
+
+namespace Deephaven.ExcelAddIn.Util;
+
+public class SequentialExecutor {
+  private readonly object _sync = new();
+  private List<Action> _todo = new();
+  private bool _serviceThreadStarted = false;
+  private Thread? _serviceThread = null;
+
+  /**
+   * Enqueue an action onto the servicing queue and start a thread pool task if
+   * one is not already running.
+   */
+  public void Enqueue(Action action) {
+    bool needToStart;
+    lock (_sync) {
+      _todo.Add(action);
+      needToStart = _serviceThreadStarted;
+      _serviceThreadStarted = true;
+    }
+
+    if (needToStart) {
+      Task.Run(ServiceLoop).Forget();
+    }
+  }
+
+  /**
+   * If the service thread is running and the caller is on that very thread,
+   * do nothing and return false.  Otherwise, call Enqueue()
+   */
+  public bool EnqueueOrNop(Action action) {
+    lock (_sync) {
+      if (ReferenceEquals(Thread.CurrentThread, _serviceThread)) {
+        // We are already running on the service thread. Do nothing and return false.
+        return false;
+      }
+    }
+    Enqueue(action);
+    // Action was enqueued on the service thread. Return true.
+    return true;
+  }
+
+  /**
+   * If the service thread is running and the caller is on that very thread,
+   * invoke the action directly. Otherwise, call Enqueue()
+   */
+  public void EnqueueOrRun(Action action) {
+    if (!EnqueueOrNop(action)) {
+      action();
+    }
+  }
+
+  /**
+   * Create a Disposable whose Dispose operation invokes Enqueue
+   */
+  public IDisposable EnqueueWhenDisposed(Action action) {
+    return ActionAsDisposable.Create(() => Enqueue(action));
+  }
+
+  /**
+ * Create a Disposable whose Dispose operation invokes EnqueueOrRun
+ */
+  public IDisposable EnqueueOrRunWhenDisposed(Action action) {
+    return ActionAsDisposable.Create(() => EnqueueOrRun(action));
+  }
+
+  private void ServiceLoop() {
+    lock (_sync) {
+      _serviceThread = Thread.CurrentThread;
+    }
+    while (true) {
+      List<Action> localTodo;
+      lock (_sync) {
+        if (_todo.Count == 0) {
+          _serviceThread = null;
+          _serviceThreadStarted = false;
+          return;
+        }
+
+        localTodo = _todo;
+        _todo = new();
+      }
+
+      foreach (var action in localTodo) {
+        try {
+          action();
+        } catch (Exception ex) {
+          Debug.WriteLine($"Swallowing exception {ex}");
+        }
+      }
+    }
+  }
+}
