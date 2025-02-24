3ac5749951194f24903f04f899fed31118f6e3bc
Corey Kosak <kosak@kosak.com>
2025-02-24 12:59:53 -0500

    blah

--- csharp/ExcelAddIn/providers/PersistentQueryDictProvider.cs
+++ csharp/ExcelAddIn/providers/PersistentQueryDictProvider.cs
@@ -13,6 +13,7 @@ internal class PersistentQueryDictProvider :
   private readonly StateManager _stateManager;
   private readonly EndpointId _endpointId;
   private readonly object _sync = new();
+  private bool _isDisposed = false;
   private IDisposable? _upstreamDisposer = null;
   private readonly VersionTracker _versionTracker = new();
   private readonly ObserverContainer<StatusOr<IReadOnlyDictionary<Int64, PersistentQueryInfoMessage>>> _observers = new();
@@ -35,33 +36,26 @@ internal class PersistentQueryDictProvider :
   }
 
   private void RemoveObserver(IObserver<StatusOr<IReadOnlyDictionary<Int64, PersistentQueryInfoMessage>>> observer) {
-    // Do not do this under lock, because we don't want to wait while holding a lock.
-    _observers.RemoveAndWait(observer, out var isLast);
-    if (!isLast) {
-      return;
-    }
-
-    // At this point we have no observers.
-    IDisposable? disp1;
     lock (_sync) {
-      disp1 = Utility.Exchange(ref _upstreamDisposer, null);
-    }
-    disp1?.Dispose();
+      _observers.Remove(observer, out var isLast);
+      if (!isLast) {
+        return;
+      }
 
-    // At this point we are not observing anything.
-    // Release our Dictionary asynchronously. (Doesn't really need to be async, but eh)
-    lock (_sync) {
+      _isDisposed = true;
+      Utility.ClearAndDispose(ref _upstreamDisposer);
       ProviderUtil.SetState(ref _dict, "[Disposing]");
     }
   }
 
   public void OnNext(StatusOr<Subscription> subscription) {
     lock (_sync) {
-      // Regardless of whether the message is a new session or a status message,
-      // we make a new cookie so that the processing thread stops feeding stale values.
-      // Even before we were called, the caller probably called Dispose on Subscription,
-      // so the thread has either exited or is about to.
-      var newCookie = _versionTracker.SetNewVersion();
+      if (_isDisposed) {
+        return;
+      }
+      // Suppress any stale notifications that happen to show up.
+      var newCookie = _versionTracker.New();
+
       if (!subscription.GetValueOrStatus(out var sub, out var status)) {
         ProviderUtil.SetStateAndNotify(ref _dict, status, _observers);
         return;
@@ -72,7 +66,7 @@ internal class PersistentQueryDictProvider :
     }
   }
 
-  private void ProcessSubscriptionStream(Subscription sub, VersionTrackerCookie cookie) {
+  private void ProcessSubscriptionStream(Subscription sub, VersionTracker.Cookie cookie) {
     Int64 version = -1;
     var wantExit = false;
     while (true) {
