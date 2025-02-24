61c6fae6e2d073692a11fd938ac34983634f67a6
Corey Kosak <kosak@kosak.com>
2025-02-23 21:33:26 -0500

    ok this is kind of sane

--- csharp/ExcelAddIn/providers/PersistentQueryDictProvider.cs
+++ csharp/ExcelAddIn/providers/PersistentQueryDictProvider.cs
@@ -1,16 +1,13 @@
 ﻿using Deephaven.DheClient.Controller;
-using Deephaven.DheClient.Session;
 using Deephaven.ExcelAddIn.Models;
 using Deephaven.ExcelAddIn.Status;
 using Deephaven.ExcelAddIn.Util;
 using Io.Deephaven.Proto.Controller;
-using System.Collections.Generic;
-using System.Collections.Immutable;
 
 namespace Deephaven.ExcelAddIn.Providers;
 
 internal class PersistentQueryDictProvider :
-  IObserver<StatusOr<SessionManager>>,
+  IObserver<StatusOr<Subscription>>,
   IObservable<StatusOr<IReadOnlyDictionary<Int64, PersistentQueryInfoMessage>>> {
   private const string UnsetDictText = "[No PQ Dict]";
   private readonly StateManager _stateManager;
@@ -18,7 +15,6 @@ internal class PersistentQueryDictProvider :
   private readonly object _sync = new();
   private IDisposable? _upstreamDisposer = null;
   private readonly VersionTracker _versionTracker = new();
-  private StatusOr<Subscription> _subscription = UnsetDictText;
   private StatusOr<IReadOnlyDictionary<Int64, PersistentQueryInfoMessage>> _dict = UnsetDictText;
   private readonly ObserverContainer<StatusOr<IReadOnlyDictionary<Int64, PersistentQueryInfoMessage>>> _observers = new();
 
@@ -27,19 +23,19 @@ internal class PersistentQueryDictProvider :
     _endpointId = endpointId;
   }
 
-  public IDisposable Subscribe(IObserver<StatusOr<SharableDict<PersistentQueryInfoMessage>>> observer) {
+  public IDisposable Subscribe(IObserver<StatusOr<IReadOnlyDictionary<Int64, PersistentQueryInfoMessage>>> observer) {
     lock (_sync) {
       _observers.Add(observer, out var isFirst);
       _observers.OnNextOne(observer, _dict);
       if (isFirst) {
-        _upstreamDisposer = _stateManager.SubscribeToSession(_endpointId);
+        _upstreamDisposer = _stateManager.SubscribeToSubscription(_endpointId);
       }
     }
 
     return ActionAsDisposable.Create(() => RemoveObserver(observer));
   }
 
-  private void RemoveObserver(IObserver<StatusOr<SharableDict<PersistentQueryInfoMessage>>> observer) {
+  private void RemoveObserver(IObserver<StatusOr<IReadOnlyDictionary<Int64, PersistentQueryInfoMessage>>> observer) {
     _observers.RemoveAndWait(observer, out var isLast);
     if (!isLast) {
       return;
@@ -52,44 +48,26 @@ internal class PersistentQueryDictProvider :
     disp1?.Dispose();
 
     lock (_sync) {
-      Background666.InvokeDispose(Utility.Exchange(ref _subscription, UnsetDictText));
+      _observers.SetState(ref _dict, UnsetDictText);
     }
   }
 
-  public void OnNext(StatusOr<SessionManager> sessionManager) {
+  public void OnNext(StatusOr<Subscription> subscription) {
     lock (_sync) {
       // Regardless of whether the message is a new session or a status message,
       // we make a new cookie so that the processing thread stops.
       var newCookie = _versionTracker.SetNewVersion();
-      if (!sessionManager.GetValueOrStatus(out var sm, out var status)) {
-        // probably have to call Subscription.Cancel() to get it to stop
-        _ = _versionTracker.SetNewVersion();
-        SetStateLocked(ref _subscription, status);
+      if (!subscription.GetValueOrStatus(out var sub, out var status)) {
         _observers.SetStateAndNotify(ref _dict, status);
         return;
       }
 
-      var sub = sm.Subscribe();
-      // Our value is "subscription" with a dependency on SessionManager
-      using var newState = StatusOr<Subscription>.OfValue(sub, sessionManager);
-      SetStateLocked(ref _subscription, newState);
-      SetStateLocked(ref _dict, StatusOr<IReadOnlyDictionary<Int64, PersistentQueryInfoMessage>>.OfValue(
-        ImmutableDictionary<Int64, PersistentQueryInfoMessage>.Empty));
-      // probably have to call Subscription.Cancel() to get it to stop
-      var sessionCopy = newState.Share();
-      Background666.Run(() => ProcessSubscriptionStream(sessionCopy, newCookie));
+      _observers.SetStateAndNotify(ref _dict, "[Processing Subscriptions]");
+      Background666.Run(() => ProcessSubscriptionStream(sub, newCookie));
     }
   }
 
-  private void SetStateLocked<T>(ref StatusOr<T> existing, StatusOr<T> newItem) {
-    Background666.InvokeDispose(existing);
-    existing = newItem.Share();
-  }
-
-  private void ProcessSubscriptionStream(StatusOr<Subscription> subCopy,
-    VersionTrackerCookie cookie) {
-    using var cleanup1 = subCopy;
-    var (sub, _) = subCopy;
+  private void ProcessSubscriptionStream(Subscription sub, VersionTrackerCookie cookie) {
     Int64 version = -1;
     var wantExit = false;
     while (true) {
---
+++ csharp/ExcelAddIn/providers/SubscriptionProvider.cs
@@ -0,0 +1,67 @@
+﻿using Deephaven.DheClient.Controller;
+using Deephaven.DheClient.Session;
+using Deephaven.ExcelAddIn.Models;
+using Deephaven.ExcelAddIn.Status;
+using Deephaven.ExcelAddIn.Util;
+
+namespace Deephaven.ExcelAddIn.Providers;
+
+internal class SubscriptionProvider :
+  IObserver<StatusOr<SessionManager>>,
+  IObservable<StatusOr<Subscription>> {
+  private const string UnsetSubText = "[No Subscription Dict]";
+  private readonly StateManager _stateManager;
+  private readonly EndpointId _endpointId;
+  private readonly object _sync = new();
+  private IDisposable? _upstreamDisposer = null;
+  private StatusOr<Subscription> _subscription = UnsetSubText;
+  private readonly ObserverContainer<StatusOr<Subscription>> _observers = new();
+
+  public SubscriptionProvider(StateManager stateManager, EndpointId endpointId) {
+    _stateManager = stateManager;
+    _endpointId = endpointId;
+  }
+
+  public IDisposable Subscribe(IObserver<StatusOr<Subscription>> observer) {
+    lock (_sync) {
+      _observers.Add(observer, out var isFirst);
+      _observers.OnNextOne(observer, _subscription);
+      if (isFirst) {
+        _upstreamDisposer = _stateManager.SubscribeToSession(_endpointId);
+      }
+    }
+
+    return ActionAsDisposable.Create(() => RemoveObserver(observer));
+  }
+
+  private void RemoveObserver(IObserver<StatusOr<Subscription>> observer) {
+    _observers.RemoveAndWait(observer, out var isLast);
+    if (!isLast) {
+      return;
+    }
+
+    IDisposable? disp1;
+    lock (_sync) {
+      disp1 = Utility.Exchange(ref _upstreamDisposer, null);
+    }
+    disp1?.Dispose();
+
+    lock (_sync) {
+      _observers.SetState(ref _subscription, "[Disposing]");
+    }
+  }
+
+  public void OnNext(StatusOr<SessionManager> sessionManager) {
+    lock (_sync) {
+      if (!sessionManager.GetValueOrStatus(out var sm, out var status)) {
+        _observers.SetStateAndNotify(ref _subscription, status);
+        return;
+      }
+
+      var sub = sm.Subscribe();
+      // NewState is Subscription with a dependency on sessionManager
+      using var newState = StatusOr<Subscription>.OfValue(sub, sessionManager);
+      _observers.SetStateAndNotify(ref _subscription, newState);
+    }
+  }
+}
