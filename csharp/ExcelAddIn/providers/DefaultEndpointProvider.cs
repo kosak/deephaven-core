082fa2c00b002d56bb082cc11ebb7d1a6489ee33
Corey Kosak <kosak@kosak.com>
2025-03-07 09:44:54 -0500

    expo

--- csharp/ExcelAddIn/providers/DefaultEndpointProvider.cs
+++ csharp/ExcelAddIn/providers/DefaultEndpointProvider.cs
@@ -3,12 +3,12 @@ using Deephaven.ExcelAddIn.Util;
 
 namespace Deephaven.ExcelAddIn.Providers;
 
-internal class DefaultEndpointProvider : IStatusObservable<EndpointId?> {
+internal class DefaultEndpointProvider : IStatusObservable<EndpointId> {
   private readonly object _sync = new();
-  private readonly ObserverContainer<EndpointId?> _observers = new();
+  private readonly ObserverContainer<EndpointId> _observers = new();
   private EndpointId? _endpointId = null;
   
-  public IDisposable Subscribe(IStatusObserver<EndpointId?> observer) {
+  public IDisposable Subscribe(IStatusObserver<EndpointId> observer) {
     lock (_sync) {
       _observers.AddAndNotify(observer, _endpointId, out _);
     }
@@ -16,7 +16,7 @@ internal class DefaultEndpointProvider : IStatusObservable<EndpointId?> {
     return ActionAsDisposable.Create(() => RemoveObserver(observer));
   }
 
-  private void RemoveObserver(IStatusObserver<EndpointId?> observer) {
+  private void RemoveObserver(IStatusObserver<EndpointId> observer) {
     lock (_sync) {
       _observers.Remove(observer, out _);
     }
