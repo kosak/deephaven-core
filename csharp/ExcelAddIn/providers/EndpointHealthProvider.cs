a5c59ef09c3523e755bdf74d7ed04f16d9a20310
Corey Kosak <kosak@kosak.com>
2025-03-07 20:35:48 -0500

    super volare time

--- csharp/ExcelAddIn/providers/EndpointHealthProvider.cs
+++ csharp/ExcelAddIn/providers/EndpointHealthProvider.cs
@@ -6,27 +6,27 @@ using Deephaven.ManagedClient;
 
 namespace Deephaven.ExcelAddIn.Providers;
 
-/**
- * The job of this class is to observe EndpointConfig notifications for a given EndpointId,
- * and then provide EndpointHealth notifications. These notifications are either a placeholder
- * EndpointHealth object (if healthy) or a status message (if not).
- *
- * The logic works as follows. Subscribe to EndpointConfigs for a given EndpointId.
- * When a valid EndpoingConfig arrives, subscribe to 
- * *
- * Observes the EndpointConfig. When a valid EndpointConfig is received, observes
- * the appropriate CoreClientProvider or CorePlusSessionProvider.
- * When those things provide responses, translate them to ConnectionHealth messages.
- * We use StatusOr&lt;ConnectionHealth&gt; . A healthy connection sends a StatusOr
- * with value set to ConnectionHealth (this is an object without any members). On the
- * other hand, an unhealthy connection sends a StatusOr with the status text sent to
- * whatever status text was received from upstream.
- */
+/// <summary>
+/// The job of this class is to observe EndpointConfig notifications for a given EndpointId,
+/// and then provide EndpointHealth notifications. These notifications are either a placeholder
+/// EndpointHealth object (if healthy) or a status message (if not).
+///
+/// The logic works as follows. Subscribe to EndpointConfigs for a given EndpointId.
+/// When a valid EndpoingConfig arrives, subscribe to 
+///
+/// Observes the EndpointConfig. When a valid EndpointConfig is received, observes
+/// the appropriate CoreClientProvider or CorePlusSessionProvider.
+/// When those things provide responses, translate them to ConnectionHealth messages.
+/// We use StatusOr&lt; ConnectionHealth & gt; . A healthy connection sends a StatusOr
+/// with value set to ConnectionHealth (this is an object without any members). On the
+/// other hand, an unhealthy connection sends a StatusOr with the status text sent to
+/// whatever status text was received from upstream.
+/// </summary>
 internal class EndpointHealthProvider :
-  IStatusObserver<EndpointConfigBase>,
-  IStatusObserver<RefCounted<Client>>,
-  IStatusObserver<RefCounted<SessionManager>>,
-  IStatusObservable<EndpointHealth>,
+  IValueObserver<StatusOr<EndpointConfigBase>>,
+  IValueObserver<StatusOr<RefCounted<Client>>>,
+  IValueObserver<StatusOr<RefCounted<SessionManager>>>,
+  IValueObservable<StatusOr<EndpointHealth>>,
   IDisposable {
   private const string UnsetHealthString = "[No Config]";
   private const string ConnectionOkString = "OK";
@@ -39,8 +39,8 @@ internal class EndpointHealthProvider :
   private readonly Latch _isDisposed = new();
   private IDisposable? _upstreamConfigDisposer = null;
   private IDisposable? _upstreamClientOrSessionDisposer = null;
+  private readonly ObserverContainer<StatusOr<EndpointHealth>> _observers = new();
   private StatusOr<EndpointHealth> _endpointHealth = UnsetHealthString;
-  private readonly ObserverContainer<EndpointHealth> _observers = new();
 
   public EndpointHealthProvider(StateManager stateManager, EndpointId endpointId) {
     _stateManager = stateManager;
@@ -51,7 +51,7 @@ internal class EndpointHealthProvider :
   /// <summary>
   /// Subscribe to connection health changes
   /// </summary>
-  public IDisposable Subscribe(IStatusObserver<EndpointHealth> observer) {
+  public IDisposable Subscribe(IValueObserver<StatusOr<EndpointHealth>> observer) {
     lock (_sync) {
       SorUtil.AddObserverAndNotify(_observers, observer, _endpointHealth, out _);
       if (_isSubscribed.TrySet()) {
@@ -59,13 +59,11 @@ internal class EndpointHealthProvider :
       }
     }
 
-    return ActionAsDisposable.Create(() => RemoveObserver(observer));
-  }
-
-  private void RemoveObserver(IStatusObserver<EndpointHealth> observer) {
-    lock (_sync) {
-      _observers.Remove(observer, out _);
-    }
+    return ActionAsDisposable.Create(() => {
+      lock (_sync) {
+        _observers.Remove(observer, out _);
+      }
+    });
   }
 
   public void Dispose() {
@@ -80,63 +78,56 @@ internal class EndpointHealthProvider :
     }
   }
 
-  void IStatusObserver<EndpointConfigBase>.OnStatus(string status) {
-    lock (_sync) {
-      if (_isDisposed.Value) {
-        return;
-      }
-      _freshness.Reset();
-      Utility.ClearAndDispose(ref _upstreamClientOrSessionDisposer);
-      SorUtil.ReplaceAndNotify(ref _endpointHealth, status, _observers);
-    }
-  }
-
-  public void OnNext(EndpointConfigBase credentials) {
+  public void OnNext(StatusOr<EndpointConfigBase> credentials) {
     lock (_sync) {
       if (_isDisposed.Value) {
         return;
       }
 
-      _freshness.Reset();
+      _freshness.Refresh();
       Utility.ClearAndDispose(ref _upstreamClientOrSessionDisposer);
+
+      if (!credentials.GetValueOrStatus(out var creds, out var status)) {
+        SorUtil.ReplaceAndNotify(ref _endpointHealth, status, _observers);
+        return;
+      }
+
+
       SorUtil.ReplaceAndNotify(ref _endpointHealth, "[Unknown]", _observers);
 
       // Upstream has core or corePlus value. Use the visitor to figure 
       // out which one and subscribe to it.
-      _upstreamClientOrSessionDisposer = credentials.AcceptVisitor(
+      _upstreamClientOrSessionDisposer = creds.AcceptVisitor(
         (CoreEndpointConfig _) => {
-          var fobs = new FreshnessObserver<RefCounted<Client>>(this, _freshness.Current);
+          var fobs = new FreshnessFilter<StatusOr<RefCounted<Client>>>(this, _freshness.Current);
           return _stateManager.SubscribeToCoreClient(_endpointId, fobs);
         },
         (CorePlusEndpointConfig _) => {
-          var fobs = new FreshnessObserver<RefCounted<SessionManager>>(this, _freshness.Current);
+          var fobs = new FreshnessFilter<StatusOr<RefCounted<SessionManager>>>(this, _freshness.Current);
           return _stateManager.SubscribeToSessionManager(_endpointId, fobs);
         });
     }
   }
 
-  void IStatusObserver<RefCounted<Client>>.OnStatus(string status) {
-    OnNextHelper(status);
+  public void OnNext(StatusOr<RefCounted<Client>> client) {
+    OnNextHelper(client);
   }
 
-  public void OnNext(RefCounted<Client> client) {
-    OnNextHelper(ConnectionOkString);
+  public void OnNext(StatusOr<RefCounted<SessionManager>> sessionManager) {
+    OnNextHelper(sessionManager);
   }
 
-  void IStatusObserver<RefCounted<SessionManager>>.OnStatus(string status) {
-    OnNextHelper(status);
-  }
-
-  public void OnNext(RefCounted<SessionManager> sm) {
-    OnNextHelper(ConnectionOkString);
-  }
-
-  private void OnNextHelper(string message) {
+  private void OnNextHelper<T>(StatusOr<T> statusOr) {
     lock (_sync) {
       if (_isDisposed.Value) {
         return;
       }
-      SorUtil.ReplaceAndNotify(ref _endpointHealth, message, _observers);
+      if (!statusOr.GetValueOrStatus(out _, out var status)) {
+        SorUtil.ReplaceAndNotify(ref _endpointHealth, status, _observers);
+        return;
+      }
+      var result = new EndpointHealth();
+      SorUtil.ReplaceAndNotify(ref _endpointHealth, result, _observers);
     }
   }
 }
