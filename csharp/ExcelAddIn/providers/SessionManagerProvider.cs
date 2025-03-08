f60bddd838fae52ae1baa84a3046eff82f19041c
Corey Kosak <kosak@kosak.com>
2025-03-07 21:07:36 -0500

    super volare time

--- csharp/ExcelAddIn/providers/SessionManagerProvider.cs
+++ csharp/ExcelAddIn/providers/SessionManagerProvider.cs
@@ -6,113 +6,108 @@ using Deephaven.ExcelAddIn.Util;
 
 namespace Deephaven.ExcelAddIn.Providers;
 
-/**
- * The job of this class is to observe EndpointConfigs for a given EndpointId,
- * and then provide SessionManagers. If the EndpointConfig does not refer to a Core
- * Plus instance, we notify an error. If it does, we try to connect to that instance in the
- * background. If that connection eventually succeeds, we notify our observers with the
- * corresponding SessionManager object. Otherwise we notify an error.
- */
+/// <summary>
+/// The job of this class is to observe EndpointConfigs for a given EndpointId,
+/// and then provide SessionManagers. If the EndpointConfig does not refer to a Core
+/// Plus instance, we notify an error. If it does, we try to connect to that instance in the
+/// background. If that connection eventually succeeds, we notify our observers with the
+/// corresponding SessionManager object. Otherwise we notify an error.
+/// </summary>
 internal class SessionManagerProvider :
-  IObserver<StatusOr<EndpointConfigBase>>,
-  IObservable<StatusOr<SessionManager>>,
+  IValueObserver<StatusOr<EndpointConfigBase>>,
+  IValueObservable<StatusOr<RefCounted<SessionManager>>>,
   IDisposable {
   private const string UnsetSessionManagerText = "[No SessionManager]";
   private readonly StateManager _stateManager;
   private readonly EndpointId _endpointId;
   private readonly object _sync = new();
-  private bool _isDisposed = false;
+  private readonly FreshnessSource _freshness;
+  private readonly Latch _isSubscribed = new();
+  private readonly Latch _isDisposed = new();
   private IDisposable? _upstreamDisposer = null;
-  private readonly ObserverContainer<StatusOr<SessionManager>> _observers = new();
-  private readonly VersionTracker _versionTracker = new();
-  private StatusOr<SessionManager> _session = UnsetSessionManagerText;
+  private readonly ObserverContainer<StatusOr<RefCounted<SessionManager>>> _observers = new();
+  private StatusOr<RefCounted<SessionManager>> _session = UnsetSessionManagerText;
 
   public SessionManagerProvider(StateManager stateManager, EndpointId endpointId) {
     _stateManager = stateManager;
     _endpointId = endpointId;
+    _freshness = new(_sync);
   }
 
   /// <summary>
   /// Subscribe to session changes
   /// </summary>
-  public IDisposable Subscribe(IObserver<StatusOr<SessionManager>> observer) {
+  public IDisposable Subscribe(IValueObserver<StatusOr<RefCounted<SessionManager>>> observer) {
     lock (_sync) {
-      _observers.AddAndNotify(observer, _session, out var isFirst);
-      if (isFirst) {
+      _observers.AddAndNotify(observer, _session, out _);
+      if (_isSubscribed.TrySet()) {
         _upstreamDisposer = _stateManager.SubscribeToEndpointConfig(_endpointId, this);
       }
     }
 
-    return ActionAsDisposable.Create(() => RemoveObserver(observer));
+    return ActionAsDisposable.Create(() => {
+      lock (_sync) {
+        _observers.Remove(observer, out _);
+      }
+    });
   }
 
-  private void RemoveObserver(IObserver<StatusOr<SessionManager>> observer) {
+  public void Dispose() {
     lock (_sync) {
-      _observers.Remove(observer, out var isLast);
-      if (!isLast) {
+      if (!_isDisposed.TrySet()) {
         return;
       }
 
-      _isDisposed = true;
       Utility.ClearAndDispose(ref _upstreamDisposer);
-      ProviderUtil.SetState(ref _session, "[Disposed]");
+      SorUtil.Replace(ref _session, "[Disposed]");
     }
   }
 
   public void OnNext(StatusOr<EndpointConfigBase> credentials) {
     lock (_sync) {
-      if (_isDisposed) {
+      if (_isDisposed.Value) {
         return;
       }
 
       // Suppress background messages that might come in after this point.
-      var cookie = _versionTracker.New();
+      _freshness.Refresh();
 
       if (!credentials.GetValueOrStatus(out var cbase, out var status)) {
-        ProviderUtil.SetStateAndNotify(ref _session, status, _observers);
+        SorUtil.ReplaceAndNotify(ref _session, status, _observers);
         return;
       }
 
       _ = cbase.AcceptVisitor(
         _ => {
           // We are a CorePlus entity but we are getting credentials for core.
-          ProviderUtil.SetStateAndNotify(ref _session,
+          SorUtil.ReplaceAndNotify(ref _session,
             "Persistent Queries are not supported in Community Core", _observers);
           return Unit.Instance;
         },
         corePlus => {
-          ProviderUtil.SetStateAndNotify(ref _session, "Trying to connect", _observers);
-          Background666.Run(() => OnNextBackground(corePlus, cookie));
+          SorUtil.ReplaceAndNotify(ref _session, "Trying to connect", _observers);
+          Background.Run(() => OnNextBackground(corePlus, _freshness.Current));
           return Unit.Instance;
         });
     }
   }
 
-  private void OnNextBackground(CorePlusEndpointConfig config,
-    VersionTracker.Cookie versionCookie) {
-    StatusOr<SessionManager> result;
+  private void OnNextBackground(CorePlusEndpointConfig config, FreshnessToken token) {
+    RefCounted<SessionManager>? smRef = null;
+    StatusOr<RefCounted<SessionManager>> result;
     try {
       var sm = EndpointFactory.ConnectToCorePlus(config);
-      result = StatusOr<SessionManager>.OfValue(sm);
+      smRef = RefCounted.Acquire(sm);
+      result = smRef;
     } catch (Exception ex) {
-      result = StatusOr<SessionManager>.OfStatus(ex.Message);
+      result = ex.Message;
     }
-    using var cleanup = result;
+    using var cleanup = smRef;
 
     lock (_sync) {
-      if (!_isDisposed && versionCookie.IsCurrent) {
-        ProviderUtil.SetStateAndNotify(ref _session, result, _observers);
+      if (!_isDisposed.Value && token.IsCurrentUnsafe) {
+        SorUtil.ReplaceAndNotify(ref _session, result, _observers);
       }
     }
-  }
-
-  public void OnCompleted() {
-    // TODO(kosak)
-    throw new NotImplementedException();
-  }
-
-  public void OnError(Exception error) {
-    // TODO(kosak)
-    throw new NotImplementedException();
   }
 }
