095d3a83a9c4a788bd24dfdc58e9ef392bdc6c5a
Corey Kosak <kosak@kosak.com>
2025-03-07 20:03:53 -0500

    ok can you live with this

--- csharp/ExcelAddIn/providers/DefaultEndpointProvider.cs
+++ csharp/ExcelAddIn/providers/DefaultEndpointProvider.cs
@@ -4,24 +4,22 @@ using Deephaven.ExcelAddIn.Util;
 
 namespace Deephaven.ExcelAddIn.Providers;
 
-internal class DefaultEndpointProvider : IStatusObservable<EndpointId> {
+internal class DefaultEndpointProvider : IValueObservable<StatusOr<EndpointId>> {
   private const string UnsetEndpointText = "[No endpoint]";
   private readonly object _sync = new();
-  private readonly ObserverContainer<EndpointId> _observers = new();
+  private readonly ObserverContainer<StatusOr<EndpointId>> _observers = new();
   private StatusOr<EndpointId> _endpointId = UnsetEndpointText;
   
-  public IDisposable Subscribe(IStatusObserver<EndpointId> observer) {
+  public IDisposable Subscribe(IValueObserver<StatusOr<EndpointId>> observer) {
     lock (_sync) {
       SorUtil.AddObserverAndNotify(_observers, observer, _endpointId, out _);
     }
 
-    return ActionAsDisposable.Create(() => RemoveObserver(observer));
-  }
-
-  private void RemoveObserver(IStatusObserver<EndpointId> observer) {
-    lock (_sync) {
-      _observers.Remove(observer, out _);
-    }
+    return ActionAsDisposable.Create(() => {
+      lock (_sync) {
+        _observers.Remove(observer, out _);
+      }
+    });
   }
 
   public void Set(EndpointId? endpointId) {
