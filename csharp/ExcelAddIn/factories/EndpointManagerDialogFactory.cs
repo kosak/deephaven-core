977761a3ebce44e1578efd155d562e989b29aae9
Corey Kosak <kosak@kosak.com>
2025-03-08 18:53:43 -0500

    qweeqwqew

--- csharp/ExcelAddIn/factories/EndpointManagerDialogFactory.cs
+++ csharp/ExcelAddIn/factories/EndpointManagerDialogFactory.cs
@@ -1,16 +1,15 @@
 ﻿using Deephaven.ExcelAddIn.Managers;
-using Deephaven.ExcelAddIn.Util;
 using Deephaven.ExcelAddIn.Views;
 
 namespace Deephaven.ExcelAddIn.Factories;
 
 internal static class EndpointManagerDialogFactory {
   public static void CreateAndShow(StateManager stateManager) {
-    Utility.RunInBackground(() => {
+    Background.Run(() => {
       var cmDialog = new EndpointManagerDialog();
       var dm = EndpointManagerDialogManager.Create(stateManager, cmDialog);
       cmDialog.Closed += (_, _) => dm.Dispose();
-      // Blocks forever (in this private thread)
+      // Blocks forever (in this dedicated thread) until the form is closed.
       cmDialog.ShowDialog(); 
     });
   }
