0afcc92c4f2e609f458e5365c86c99c290a80673
Corey Kosak <kosak@kosak.com>
2025-03-14 02:03:02 -0400

    fine

--- csharp/ExcelAddIn/status/StatusOr.cs
+++ csharp/ExcelAddIn/status/StatusOr.cs
@@ -2,8 +2,13 @@
 
 namespace Deephaven.ExcelAddIn.Status;
 
+public sealed record Status(string Text, bool IsState) {
+  public static Status OfProgress(string text) => new Status(text, false);
+  public static Status OfState(string text) => new Status(text, true);
+}
+
 public sealed class StatusOr<T> {
-  private readonly string? _status;
+  private readonly Status? _status;
   private readonly T? _value;
 
   public static implicit operator StatusOr<T>(string status) {
@@ -14,32 +19,36 @@ public sealed class StatusOr<T> {
     return OfValue(value);
   }
 
-  public static StatusOr<T> OfStatus(string status) {
-    return new StatusOr<T>(status, default);
+  public static StatusOr<T> OfProgress(string progress) {
+    return new StatusOr<T>(Status.OfProgress(progress), default);
+  }
+
+  public static StatusOr<T> OfState(string state) {
+    return new StatusOr<T>(Status.OfState(state), default);
   }
 
   public static StatusOr<T> OfValue(T value) {
     return new StatusOr<T>(null, value);
   }
 
-  private StatusOr(string? status, T? value) {
+  private StatusOr(Status? status, T? value) {
     _status = status;
     _value = value;
   }
 
   public bool GetValueOrStatus(
     [NotNullWhen(true)]out T? value,
-    [NotNullWhen(false)]out string? status) {
+    [NotNullWhen(false)]out Status? status) {
     status = _status;
     value = _value;
     return status == null;
   }
 
-  public U AcceptVisitor<U>(Func<T, U> onValue, Func<string, U> onStatus) {
+  public U AcceptVisitor<U>(Func<T, U> onValue, Func<Status, U> onStatus) {
     return _status == null ? onValue(_value!) : onStatus(_status);
   }
 
-  public void Deconstruct(out T? value, out string? status) {
+  public void Deconstruct(out T? value, out Status? status) {
     value = _value;
     status = _status;
   }
