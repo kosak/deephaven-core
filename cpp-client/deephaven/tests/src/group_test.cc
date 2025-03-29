cabc3722a7fcf83d6bbf7455114e494d1f15cfe5
Corey Kosak <coreykosak@deephaven.io>
2025-03-29 00:14:22 -0400

    where is it

--- cpp-client/deephaven/tests/src/group_test.cc
+++ cpp-client/deephaven/tests/src/group_test.cc
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2016-2025 Deephaven Data Labs and Patent Pending
  */
+#include <cstdint>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -8,41 +9,37 @@
 #include "deephaven/third_party/catch.hpp"
 #include "deephaven/tests/test_util.h"
 #include "deephaven/client/client.h"
+#include "deephaven/client/utility/table_maker.h"
 #include "deephaven/dhcore/container/row_sequence.h"
 
+using deephaven::client::utility::TableMaker;
+using deephaven::dhcore::chunk::BooleanChunk;
+using deephaven::dhcore::chunk::ContainerBaseChunk;
 using deephaven::dhcore::container::RowSequence;
 
 namespace deephaven::client::tests {
 TEST_CASE("Group a Table", "[group]") {
   auto tm = TableMakerForTests::Create();
 
-  std::vector<std::string> type_data = {
+  TableMaker maker;
+  maker.AddColumn<std::string>("Type", {
       "Granny Smith",
       "Granny Smith",
       "Gala",
       "Gala",
       "Golden Delicious",
       "Golden Delicious"
-  };
-
-  std::vector<std::string> color_data = {
+  });
+  maker.AddColumn<std::string>("Color", {
       "Green", "Green", "Red-Green", "Orange-Green", "Yellow", "Yellow"
-  };
-
-  std::vector<int32_t> weight_data = {
+  });
+  maker.AddColumn<int32_t>("Weight", {
       // 102, 85, 79, 92, 78, 99
       0, 0, 0, 1, 1, 1
-  };
-
-  std::vector<int32_t> calorie_data = {
+  });
+  maker.AddColumn<int32_t>("Calories", {
       53, 48, 51, 61, 46, 57
-  };
-
-  TableMaker maker;
-  maker.AddColumn("Type", type_data);
-  maker.AddColumn("Color", color_data);
-  maker.AddColumn("Weight", weight_data);
-  maker.AddColumn("Calories", calorie_data);
+  });
   auto t1 = maker.MakeTable(tm.Client().GetManager());
 
   auto grouped = t1.By("Weight");
