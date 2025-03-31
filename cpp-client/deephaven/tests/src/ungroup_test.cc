808fcdf959372100a2e913a4389b0b4acabbcb81
Corey Kosak <coreykosak@deephaven.io>
2025-03-30 21:10:38 -0400

    ok tests are pretty good

--- cpp-client/deephaven/tests/src/ungroup_test.cc
+++ cpp-client/deephaven/tests/src/ungroup_test.cc
@@ -16,17 +16,20 @@ TEST_CASE("Ungroup columns", "[ungroup]") {
   auto by_table = table.Where("Ticker == `AAPL`").View("Ticker", "Close").By("Ticker");
   std::cout << by_table.Stream(true) << '\n';
   auto ungrouped = by_table.Ungroup("Close");
+  std::cout << ungrouped.Stream(true) << '\n';
 
-  TableMaker expected1;
-  expected1.AddColumn<std::string>("Ticker", {"AAPL"});
-  expected1.AddColumn<std::string>("Close", {"[23.5,24.2,26.7]"});
-  TableComparerForTests::Compare(expected1, by_table);
+  {
+    TableMaker expected;
+    expected.AddColumn<std::string>("Ticker", {"AAPL"});
+    expected.AddColumn<std::vector<double>>("Close", {{23.5, 24.2, 26.7}});
+    TableComparerForTests::Compare(expected, by_table);
+  }
 
-  std::vector<std::string> ug_ticker_data = {"AAPL", "AAPL", "AAPL"};
-  std::vector<double> ug_close_data = {23.5, 24.2, 26.7};
-  TableMaker expected2;
-  expected1.AddColumn<std::string>("Ticker", {"AAPL", "AAPL", "AAPL"});
-  expected1.AddColumn<double>("Close", {23.5, 24.2, 26.7});
-  TableComparerForTests::Compare(expected2, ungrouped);
+  {
+    TableMaker expected;
+    expected.AddColumn<std::string>("Ticker", {"AAPL", "AAPL", "AAPL"});
+    expected.AddColumn<double>("Close", {23.5, 24.2, 26.7});
+    TableComparerForTests::Compare(expected, ungrouped);
+  }
 }
 }  // namespace deephaven::client::tests
