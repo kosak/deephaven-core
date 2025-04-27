4383b6e9bb65c29359ce4f33d120b5b0e5b6c1ba
Corey Kosak <coreykosak@deephaven.io>
2025-04-26 22:56:15 -0400

    ok fine now what

---
+++ cpp-client/deephaven/dhcore/include/public/deephaven/dhcore/column/container_column_source.h
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2016-2025 Deephaven Data Labs and Patent Pending
+ */
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <utility>
+
+#include "deephaven/dhcore/chunk/chunk.h"
+#include "deephaven/dhcore/chunk/chunk_traits.h"
+#include "deephaven/dhcore/column/column_source.h"
+#include "deephaven/dhcore/column/column_source_utils.h"
+#include "deephaven/dhcore/container/container.h"
+#include "deephaven/dhcore/container/row_sequence.h"
+#include "deephaven/dhcore/types.h"
+
+namespace deephaven::dhcore::column {
+template<typename T, template<typename> typename TBase>
+class ContainerColumnSource final : public TBase<T>,
+  public std::enable_shared_from_this<ContainerColumnSource<T, TBase>> {
+  struct Private {
+  };
+  using BooleanChunk = deephaven::dhcore::chunk::BooleanChunk;
+  using Chunk = deephaven::dhcore::chunk::Chunk;
+  using UInt64Chunk = deephaven::dhcore::chunk::UInt64Chunk;
+  using ColumnSourceImpls = deephaven::dhcore::column::ColumnSourceImpls;
+  using ColumnSourceVisitor = deephaven::dhcore::column::ColumnSourceVisitor;
+  using ContainerBase = deephaven::dhcore::container::ContainerBase;
+  using RowSequence = deephaven::dhcore::container::RowSequence;
+
+  template<typename U>
+  using Container = deephaven::dhcore::container::Container<U>;
+
+public:
+  static std::shared_ptr<ContainerColumnSource> Create(const ElementType &element_type,
+    std::shared_ptr<Container<T>> container);
+
+  explicit ContainerColumnSource(Private, const ElementType &element_type,
+    std::shared_ptr<Container<T>> container) : element_type_(element_type),
+    container_(std::move(container)) {}
+  ~ContainerColumnSource() final = default;
+
+  void FillChunk(const RowSequence &rows, Chunk *dest, BooleanChunk *optional_dest_null_flags) const final {
+    using chunkType_t = typename deephaven::dhcore::chunk::TypeToChunk<T>::type_t;
+    using deephaven::dhcore::utility::VerboseCast;
+    size_t dest_index = 0;
+    const auto *data = container_->data();
+    auto *typed_dest = VerboseCast<chunkType_t*>(dest);
+    rows.ForEachInterval([&](uint64_t begin_key, uint64_t end_key) {
+      for (auto current = begin_key; current != end_key; ++current) {
+        if (container_->IsNull(current)) {
+          if (optional_dest_null_flags != nullptr) {
+            (*optional_dest_null_flags)[dest_index] = true;
+          }
+        } else {
+          typed_dest[dest_index] = data[current];
+        }
+      }
+    });
+
+    ColumnSourceImpls::FillChunk<chunkType_t>(rows, dest, optional_dest_null_flags, data_);
+  }
+
+  void FillChunkUnordered(const UInt64Chunk &row_keys, Chunk *dest,
+      BooleanChunk *optional_dest_null_flags) const final {
+    const char *message = "FillChunkUnordered is not implemented yet";
+    throw std::runtime_error(DEEPHAVEN_LOCATION_STR(message));
+  }
+
+  [[nodiscard]]
+  const ElementType &GetElementType() const final {
+    return element_type_;
+  }
+
+  void AcceptVisitor(ColumnSourceVisitor *visitor) const final {
+    visitor->Visit(*this);
+  }
+
+private:
+  ElementType element_type_;
+  std::shared_ptr<Container<T>> container_;
+};
+
+using Int8ContainerColumnSource = ContainerColumnSource<int8_t, NumericColumnSource>;
+using Int16ContainerColumnSource = ContainerColumnSource<int16_t, NumericColumnSource>;
+using Int32ContainerColumnSource = ContainerColumnSource<int32_t, NumericColumnSource>;
+using Int64ContainerColumnSource = ContainerColumnSource<int64_t, NumericColumnSource>;
+using FloatContainerColumnSource = ContainerColumnSource<float, NumericColumnSource>;
+using DoubleContainerColumnSource = ContainerColumnSource<double, NumericColumnSource>;
+
+using BooleanContainerColumnSource = ContainerColumnSource<bool, GenericColumnSource>;
+using StringContainerColumnSource = ContainerColumnSource<std::string, GenericColumnSource>;
+using DateTimeContainerColumnSource = ContainerColumnSource<DateTime, GenericColumnSource>;
+using LocalDateContainerColumnSource = ContainerColumnSource<LocalDate, GenericColumnSource>;
+using LocalTimeContainerColumnSource = ContainerColumnSource<LocalTime, GenericColumnSource>;
+}  // namespace deephaven::dhcore::column
