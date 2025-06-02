7694d22734318af2c9f4f32e50362a1970ef84bd
Corey Kosak <kosak@kosak.com>
2025-06-01 23:46:29 -0400

    super fan

--- csharp/ExcelAddIn/util/StatusOrUtil.cs
+++ csharp/ExcelAddIn/util/StatusOrUtil.cs
@@ -1,10 +1,12 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using Deephaven.ExcelAddIn.Observable;
+using Deephaven.ExcelAddIn.Providers;
 
 namespace Deephaven.ExcelAddIn.Util;
 
 public class StatusOrHolder<T> {
   private StatusOr<T> _value;
+  private IDisposable? _sharedDisposer = null;
 
   public StatusOrHolder(string state) {
     _value = StatusOr<T>.OfFixed(state);
@@ -20,29 +22,17 @@ public class StatusOrHolder<T> {
   }
 
   /// <summary>
-  /// First discards the old value of dest (which will dispose it if it contains
-  /// a value, and that value is a RefCounted type).
-  /// Then if newValue contains a value and that value is RefCounted, calls Share()
-  /// to share that RefCounted item, and stores the resultant StatusOr. Otherwise,
-  /// just stores newValue. The rationale here is that StatusOr acts like an
-  /// immutable type unless it contains an IDisposable or a RefCounted, in which
-  /// case it participates in the RefCounted protocol.
+  /// First unshares the old value by calling its disposer (if it exists). Then if
+  /// newValue contains a value and that value is IDisposable, then assume it is
+  /// participating in our Sharing protocol, so call Repository.Share() on it.
   /// </summary>
   public void Replace(StatusOr<T> newValue) {
-    if (_value.GetValueOrStatus(out var dv, out _) && dv is RefCounted destRc) {
-      if (ReferenceEquals(dv, newValue)) {
-        return;
-      }
-      Background.InvokeDispose(destRc);
-    }
+    Utility.ClearAndDispose(ref _sharedDisposer);
 
-    if (newValue.GetValueOrStatus(out var sv, out _) && sv is RefCounted srcSc) {
-      _value = (T)(object)srcSc.Share();
-    } else {
-      // For other cases (status-containing, or value containing but not RefCounted)
-      // you can treat the StatusOr as an freely-copyable value type.
-      _value = newValue;
+    if (newValue.GetValueOrStatus(out var nv, out _) && nv is IDisposable disp) {
+      _sharedDisposer = Repository.Share(disp);
     }
+    _value = newValue;
   }
 
   public void Notify(ObserverContainer<StatusOr<T>> observers) {
@@ -64,14 +54,18 @@ public class StatusOrHolder<T> {
   }
 
   private void EnqueueKeepAlive(ObserverContainer<StatusOr<T>> observers) {
-    if (!_value.GetValueOrStatus(out var value, out _) || value is not RefCounted rc) {
+    // We are, so we need to reshare it
+    if (!_value.GetValueOrStatus(out var value, out _) || value is not IDisposable disp) {
       return;
     }
-    // If item contains a value, and if that value is a RefCounted type, then
-    // Share() the value and post its disposer to the ObserverContainer. Since
-    // the ObserverContainer processes items in order, this will keep the
-    // previously-posted values alive until at least this point.
-    var shared = rc.Share();
-    observers.EnqueueAction(() => Background.InvokeDispose(shared));
+
+    // We are holding a sharable value, so we share it and post the disposer to
+    // the ObserverContainer. This keeps the previously-posted values alive until
+    // at least the point of the posting. We say "at least" because the Action we
+    // post is a Background action, so it will actually run at some later point.
+    // We do this because we don't want a long-running disposer to hold up the
+    // ObserverContainer.
+    var sharedDisposer = Repository.Share(disp);
+    observers.EnqueueAction(() => Background.InvokeDispose(sharedDisposer));
   }
 }
