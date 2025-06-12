14bad92fb858d9ba431844ceac6020dfec4b1e21
Corey Kosak <kosak@kosak.com>
2025-06-12 19:51:37 -0400

    arggh

--- csharp/ExcelAddIn/providers/endpoint/EndpointDictProvider.cs
+++ csharp/ExcelAddIn/providers/endpoint/EndpointDictProvider.cs
@@ -1,20 +1,34 @@
 ﻿using Deephaven.ExcelAddIn.Models;
 using Deephaven.ExcelAddIn.Observable;
 using Deephaven.ExcelAddIn.Util;
+using System;
 
 namespace Deephaven.ExcelAddIn.Providers;
 
 internal class EndpointDictProvider :
-  IValueObservable<SharableDict<EndpointConfigBase>> {
+  IValueObservable<SharableDict<EndpointConfigBase>>,
+  IValueObservable<StatusOr<EndpointId>> {
+  private const string UnsetEndpointText = "No endpoint";
+
   private readonly object _sync = new();
-  private readonly ObserverContainer<SharableDict<EndpointConfigBase>> _observers = new();
+  private readonly ObserverContainer<SharableDict<EndpointConfigBase>> _dictObservers = new();
+  private readonly ObserverContainer<StatusOr<EndpointId>> _defaultEpObservers = new();
   private SharableDict<EndpointConfigBase> _dict = new();
   private readonly Dictionary<EndpointId, Int64> _idToKey = new();
   private Int64 _nextFreeId = 0;
+  private readonly StatusOrHolder<EndpointId> _endpointId = new(UnsetEndpointText);
 
   public IObservableCallbacks Subscribe(IValueObserver<SharableDict<EndpointConfigBase>> observer) {
     lock (_sync) {
-      _observers.AddAndNotify(observer, _dict, out _);
+      _dictObservers.AddAndNotify(observer, _dict, out _);
+    }
+
+    return ObservableCallbacks.Create(Retry, () => RemoveObserver(observer));
+  }
+
+  public IObservableCallbacks Subscribe(IValueObserver<StatusOr<EndpointId>> observer) {
+    lock (_sync) {
+      _endpointId.AddObserverAndNotify(_defaultEpObservers, observer, out _);
     }
 
     return ObservableCallbacks.Create(Retry, () => RemoveObserver(observer));
@@ -26,7 +40,13 @@ internal class EndpointDictProvider :
 
   private void RemoveObserver(IValueObserver<SharableDict<EndpointConfigBase>> observer) {
     lock (_sync) {
-      _observers.Remove(observer, out _);
+      _dictObservers.Remove(observer, out _);
+    }
+  }
+
+  private void RemoveObserver(IValueObserver<StatusOr<EndpointId>> observer) {
+    lock (_sync) {
+      _defaultEpObservers.Remove(observer, out _);
     }
   }
 
@@ -75,4 +95,23 @@ internal class EndpointDictProvider :
   }
 
   public SharableDict<EndpointConfigBase> GetDict() => _dict;
+
+  public EndpointId? DefaultEndpoint {
+    get {
+      lock (_sync) {
+        return _endpointId.GetValueOrStatus(out var value, out _) ? value : null;
+      }
+    }
+  }
+
+  public void SetDefaultEndpoint(EndpointId? endpointId) {
+    lock (_sync) {
+      if (endpointId == null) {
+        _endpointId.ReplaceAndNotify(UnsetEndpointText, _observers);
+      } else {
+        _endpointId.ReplaceAndNotify(endpointId, _observers);
+      }
+    }
+  }
+
 }
