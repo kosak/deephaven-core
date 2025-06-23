3f108aa91a43663ce0e4c4e64c1494afb21dc6ed
Corey Kosak <kosak@kosak.com>
2025-06-22 23:46:09 -0400

    23

--- csharp/client/Dh_NetClientTests/LastByTest.cs
+++ csharp/client/Dh_NetClientTests/LastByTest.cs
@@ -1,21 +1,14 @@
-﻿#if false
-using Deephaven.DhClientTests;
-using Deephaven.ManagedClient;
+﻿using Deephaven.Dh_NetClient;
 using Xunit.Abstractions;
 
 namespace Deephaven.Dh_NetClientTests;
 
-public class LastByTest {
-  private readonly ITestOutputHelper _output;
-
-  public LastByTest(ITestOutputHelper output) {
-    _output = output;
-  }
+public class LastByTest(ITestOutputHelper output) {
+  private readonly ITestOutputHelper _output = output;
 
   [Fact]
   public void TestLastBy() {
     using var ctx = CommonContextForTests.Create(new ClientOptions());
-    var tm = ctx.Client.Manager;
     var testTable = ctx.TestTable;
 
     using var lb = testTable.Where("ImportDate == `2017-11-01`")
@@ -28,11 +21,11 @@ public class LastByTest {
 
     var openData = new[] { 50.5, 92.3, 40.1, 681.43, 31.5, 685.3 };
     var closeData = new[] { 53.8, 88.5, 38.7, 453, 26.7, 544.9 };
-    var tc = new TableComparer();
-    tc.AddColumn("Ticker", tickerData);
-    tc.AddColumn("Open", openData);
-    tc.AddColumn("Close", closeData);
-    tc.AssertEqualTo(lb);
+    var expected = new TableMaker();
+    expected.AddColumn("Ticker", tickerData);
+    expected.AddColumn("Open", openData);
+    expected.AddColumn("Close", closeData);
+
+    TableComparer.AssertSame(expected, lb);
   }
 }
-#endif
