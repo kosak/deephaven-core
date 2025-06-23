d4b16059efcf4f56c9143bd7ed5cabdadae5a4ef
Corey Kosak <kosak@kosak.com>
2025-06-23 12:40:20 -0400

    ouchie

---
+++ csharp/client/Dh_NetClient/column/ColumnSourceConverter.cs
@@ -0,0 +1,17 @@
+﻿namespace Deephaven.Dh_NetClient;
+
+public static class ColumnSourceConverter {
+  public static Array ToArray(IColumnSource columnSource, int numRows) {
+    var visitor = new ToArrayVisitor(numRows);
+    columnSource.Accept(visitor);
+    return visitor.Result;
+  }
+
+  private class ToArrayVisitor(int numRows) : IColumnSourceVisitor {
+    public Array Result = Array.Empty<object>();
+
+    public void Visit(IColumnSource cs) {
+      throw new Exception($"ToArray not supported for type {cs.GetType().Name}");
+    }
+  }
+}
