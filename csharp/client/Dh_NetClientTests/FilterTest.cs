cafca5be383fa1e318d98957c6f8f72cd6c8ff6f
Corey Kosak <kosak@kosak.com>
2025-06-22 22:26:02 -0400

    FilterTest

--- csharp/client/Dh_NetClientTests/FilterTest.cs
+++ csharp/client/Dh_NetClientTests/FilterTest.cs
@@ -1,17 +1,9 @@
-﻿#if false
-using Deephaven.DhClientTests;
-using Deephaven.ManagedClient;
+﻿using Deephaven.Dh_NetClient;
 using Xunit.Abstractions;
 
 namespace Deephaven.Dh_NetClientTests;
 
-public class FilterTest {
-  private readonly ITestOutputHelper _output;
-
-  public FilterTest(ITestOutputHelper output) {
-    _output = output;
-  }
-
+public class FilterTest(ITestOutputHelper output) {
   [Fact]
   public void TestFilterATable() {
     using var ctx = CommonContextForTests.Create(new ClientOptions());
@@ -19,21 +11,15 @@ public class FilterTest {
 
     var t1 = table.Where(
       "ImportDate == `2017-11-01` && Ticker == `AAPL` && (Close <= 120.0 || isNull(Close))");
-    _output.WriteLine(t1.ToString(true));
+    output.WriteLine(t1.ToString(true));
 
-    var importDateData = new[] { "2017-11-01", "2017-11-01", "2017-11-01"};
-    var tickerData = new []{ "AAPL", "AAPL", "AAPL"};
-    var openData = new[] { 22.1, 26.8, 31.5 };
-    var closeData = new[] { 23.5, 24.2, 26.7 };
-    var volData = new Int64[] { 100000, 250000, 19000 };
+    var expected = new TableMaker();
+    expected.AddColumn("ImportDate", ["2017-11-01", "2017-11-01", "2017-11-01"]);
+    expected.AddColumn("Ticker", ["AAPL", "AAPL", "AAPL"]);
+    expected.AddColumn("Open", [22.1, 26.8, 31.5]);
+    expected.AddColumn("Close", [23.5, 24.2, 26.7]);
+    expected.AddColumn("Volume", [(Int64)100000, 250000, 19000]);
 
-    var tc = new TableComparer();
-    tc.AddColumn("ImportDate", importDateData);
-    tc.AddColumn("Ticker", tickerData);
-    tc.AddColumn("Open", openData);
-    tc.AddColumn("Close", closeData);
-    tc.AddColumn("Volume", volData);
-    tc.AssertEqualTo(t1);
+    TableComparer.AssertSame(expected, t1);
   }
 }
-#endif
