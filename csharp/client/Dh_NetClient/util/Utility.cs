d8aab75729dd77f45fb5cee4e63488ef795febba
Corey Kosak <coreykosak@deephaven.io>
2025-06-29 13:08:03 -0400

    feat(csharp/client/Dh_NetClient): DH-18579: Put C# Core Client into Core Repo

---
+++ csharp/client/Dh_NetClient/util/Utility.cs
@@ -0,0 +1,24 @@
+﻿namespace Deephaven.Dh_NetClient;
+
+public static class Utility {
+  public static string FriendlyTypeName(Type t) {
+    var sw = new StringWriter();
+    FriendlyTypeNameRecurse(t, sw);
+    return sw.ToString();
+  }
+
+  private static void FriendlyTypeNameRecurse(Type t, StringWriter sw) {
+    sw.Write(t.Name);
+    if (!t.IsGenericType) {
+      return;
+    }
+    sw.Write('<');
+    string separator = "";
+    foreach (var arg in t.GetGenericArguments()) {
+      sw.Write(separator);
+      separator = ",";
+      FriendlyTypeNameRecurse(arg, sw);
+    }
+    sw.Write('>');
+  }
+}
