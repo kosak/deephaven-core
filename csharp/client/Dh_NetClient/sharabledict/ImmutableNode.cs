c467dbb47d08ccfe112b527a23e1ce97454112d0
Corey Kosak <coreykosak@deephaven.io>
2025-08-14 22:50:08 -0400

    what went wrong here

---
+++ csharp/client/Dh_NetClient/sharabledict/ImmutableNode.cs
@@ -0,0 +1,101 @@
+﻿
+//
+// Copyright (c) 2016-2025 Deephaven Data Labs and Patent Pending
+//
+namespace Deephaven.Dh_NetClient;
+
+public class ImmutableNode<T> : INode<ImmutableNode<T>, T> where T : INode<ImmutableNode<T>, T> {
+  public static ImmutableNode<T> EmptyInstance { get; } = new();
+
+  public bool TryGetChild(int childIndex, out T child) {
+    throw new NotImplementedException();
+  }
+
+  public static ImmutableNode<T> OfArray64(ReadOnlySpan<T> children) {
+    var validitySet = new Bitset64();
+    var count = 0;
+    for (var i = 0; i != children.Length; ++i) {
+      var child = children[i];
+      if (child.IsEmpty) {
+        continue;
+      }
+      validitySet = validitySet.WithElement(i);
+      count += child.Count;
+    }
+    return Create(validitySet, count, children, 0, children[0]);
+  }
+
+  public static ImmutableNode<T> Create(Bitset64 validitySet, int subtreeCount,
+    ReadOnlySpan<T> children, int replacementIndex, T replacementChild) {
+    if (validitySet.IsEmpty) {
+      return EmptyInstance;
+    }
+    return new ImmutableNode<T>(validitySet, subtreeCount, children, replacementIndex,
+      replacementChild);
+  }
+
+  public int Count { get; init; }
+  public readonly Bitset64 ValiditySet;
+  public readonly Array64<T> Children;
+
+  private ImmutableNode() {
+    ((Span<T>)Children).Fill(T.EmptyInstance);
+  }
+
+  private ImmutableNode(Bitset64 validitySet, int count, ReadOnlySpan<T> children,
+    int replacementIndex, T replacementChild) {
+    Count = count;
+    ValiditySet = validitySet;
+    children.CopyTo(Children);
+    Children[replacementIndex] = replacementChild;
+  }
+
+  public ImmutableNode<T> With(int index, T child) {
+    if (child.IsEmpty) {
+      return Without(index);
+    }
+    var newVs = ValiditySet.WithElement(index);
+    var newCount = Count - Children[index].Count + child.Count;
+    return Create(newVs, newCount, Children, index, child);
+  }
+
+  public ImmutableNode<T> Without(int index) {
+    var newVs = ValiditySet.WithoutElement(index);
+    var newCount = Count - Children[index].Count;
+    return Create(newVs, newCount, Children, index, T.EmptyInstance);
+  }
+
+  public (ImmutableNode<T>, ImmutableNode<T>, ImmutableNode<T>) CalcDifference(ImmutableNode<T> target) {
+    if (this == target) {
+      // Source and target are the same. No changes
+      return (EmptyInstance, EmptyInstance, EmptyInstance);  // added, removed, modified
+    }
+    if (this == EmptyInstance) {
+      // Relative to an empty source, everything in target was added
+      return (target, EmptyInstance, EmptyInstance);  // added, removed, modified
+    }
+    if (target == EmptyInstance) {
+      // Relative to an empty destination, everything in src was removed
+      return (EmptyInstance, this, EmptyInstance);  // added, removed, modified
+    }
+    // Need to recurse to all children to build new nodes
+    Array64<T> addedChildren = new();
+    Array64<T> removedChildren = new();
+    Array64<T> modifiedChildren = new();
+
+    var length = ((ReadOnlySpan<T>)Children).Length;
+    for (var i = 0; i != length; ++i) {
+      var (a, r, m) = Children[i].CalcDifference(target.Children[i]);
+      addedChildren[i] = a;
+      removedChildren[i] = r;
+      modifiedChildren[i] = m;
+    }
+
+    var aResult = OfArray64(addedChildren);
+    var rResult = OfArray64(removedChildren);
+    var mResult = OfArray64(modifiedChildren);
+    return (aResult, rResult, mResult);
+  }
+
+  public bool IsEmpty => ValiditySet.IsEmpty;
+}
---
+++ csharp/client/Dh_NetClient/sharabledict/SharableDict.cs
@@ -0,0 +1,157 @@
+﻿//
+// Copyright (c) 2016-2025 Deephaven Data Labs and Patent Pending
+//
+namespace Deephaven.Dh_NetClient;
+
+using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+public struct ZamboniWrap<T> : INode<ZamboniWrap<T>, T> {
+  public readonly T Value;
+
+  public ZamboniWrap(T value) {
+    Value = value;
+  }
+
+  public static ZamboniWrap<T> EmptyInstance { get; }
+  public int Count { get; }
+  public (ZamboniWrap<T>, ZamboniWrap<T>, ZamboniWrap<T>) CalcDifference(ZamboniWrap<T> target) {
+    throw new NotImplementedException();
+  }
+
+  public bool TryGetChild(int childIndex, out T child) {
+    throw new NotImplementedException();
+  }
+
+  public bool IsEmpty { get; }
+}
+
+public class SharableDict<TValue> : IReadOnlyDictionary<Int64, TValue> {
+  public static readonly SharableDict<TValue> Empty = new();
+
+  private readonly ImmutableNode<ImmutableNode<ImmutableNode<ImmutableNode<ImmutableNode<ImmutableNode<ImmutableNode<ImmutableNode<ImmutableNode<ImmutableNode<ImmutableNode<ZamboniWrap<TValue>>>>>>>>>>>> _root;
+
+  public SharableDict() {
+    _root = ImmutableNode<ImmutableNode<ImmutableNode<ImmutableNode<ImmutableNode<ImmutableNode<ImmutableNode<ImmutableNode<ImmutableNode<ImmutableNode<ImmutableNode<ZamboniWrap<TValue>>>>>>>>>>>>.Empty;
+  }
+
+  public SharableDict(
+    ImmutableNode<ImmutableNode<ImmutableNode<ImmutableNode<ImmutableNode<ImmutableNode<ImmutableNode<ImmutableNode<ImmutableNode<ImmutableNode<ImmutableNode<ZamboniWrap<TValue>>>>>>>>>>>>
+      root) {
+    _root = root;
+  }
+
+  public SharableDict<TValue> With(Int64 key, TValue value) {
+    var s = new Destructured<TValue>(_root, key);
+    var wrappedValue = new ZamboniWrap<TValue>(value);
+    var newLeaf = s.Leaf.With(s.LeafIndex, wrappedValue);
+    return s.RebuildWith(newLeaf);
+  }
+
+  public SharableDict<TValue> Without(Int64 key) {
+    var s = new Destructured<TValue>(_root, key);
+    var newLeaf = s.Leaf.Without(s.LeafIndex);
+    return s.RebuildWith(newLeaf);
+  }
+
+  public bool TryGetValue(Int64 key, [MaybeNullWhen(false)] out TValue value) {
+    var s = new Destructured<TValue>(_root, key);
+    if (!s.Leaf.TryGetChild(s.LeafIndex, out var wrappedValue)) {
+      value = default;
+      return false;
+    }
+    value = wrappedValue.Value;
+    return true;
+  }
+
+  public bool ContainsKey(Int64 key) {
+    return TryGetValue(key, out _);
+  }
+
+  public (SharableDict<TValue>, SharableDict<TValue>, SharableDict<TValue>)
+    CalcDifference(SharableDict<TValue> target) {
+    var (added, removed, modified) = _root.CalcDifference(target._root);
+    var aResult = new SharableDict<TValue>(added);
+    var rResult = new SharableDict<TValue>(removed);
+    var mResult = new SharableDict<TValue>(modified);
+    return (aResult, rResult, mResult);
+  }
+
+  public TValue this[Int64 key] {
+    get {
+      if (!TryGetValue(key, out var value)) {
+        throw new KeyNotFoundException();
+      }
+
+      return value;
+    }
+  }
+
+  public IEnumerable<Int64> Keys => this.Select(kvp => kvp.Key);
+  public IEnumerable<TValue> Values => this.Select(kvp => kvp.Value);
+
+  public IEnumerator<KeyValuePair<Int64, TValue>> GetEnumerator() {
+    // This could be written more nicely and recursively as a bunch of nested iterators,
+    // but the overhead of fetching each element would be pretty high, as each iterator
+    // would call the MoveNext of the next iterator, etc.
+    // Manually unrolling the structure into these nested foreach is a little bit homely
+    // but allows for more efficient code.
+    foreach (var i0 in _root.ValiditySet) {
+      var child0 = _root.Children[i0];
+      foreach (var i1 in child0.ValiditySet) {
+        var child1 = child0.Children[i1];
+        foreach (var i2 in child1.ValiditySet) {
+          var child2 = child1.Children[i2];
+          foreach (var i3 in child2.ValiditySet) {
+            var child3 = child2.Children[i3];
+            foreach (var i4 in child3.ValiditySet) {
+              var child4 = child3.Children[i4];
+              foreach (var i5 in child4.ValiditySet) {
+                var child5 = child4.Children[i5];
+                foreach (var i6 in child5.ValiditySet) {
+                  var child6 = child5.Children[i6];
+                  foreach (var i7 in child6.ValiditySet) {
+                    var child7 = child6.Children[i7];
+                    foreach (var i8 in child7.ValiditySet) {
+                      var child8 = child7.Children[i8];
+                      foreach (var i9 in child8.ValiditySet) {
+                        var child9 = child8.Children[i9];
+                        foreach (var i10 in child9.ValiditySet) {
+                          var data = child9.Children[i10].Value;
+                          var offset = Splitter.Merge(i0, i1, i2, i3, i4, i5, i6, i7, i8, i9, i10);
+                          yield return KeyValuePair.Create(offset, data!);
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+  public int Count => _root.Count;
+
+  public override string ToString() {
+    return string.Join(", ", this.Select(kvp => $"{kvp.Key}: {kvp.Value}"));
+  }
+}
+
+public interface INode<TSelf, TChild> {
+  static abstract TSelf EmptyInstance { get; }
+  int Count { get; }
+  (TSelf, TSelf, TSelf) CalcDifference(TSelf target);
+  bool TryGetChild(int childIndex, out TChild child);
+  bool IsEmpty { get; }
+}
+
+[InlineArray(64)]
+public struct Array64<T> {
+  public T Item;
+}
