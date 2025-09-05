de4afea0555ccd0169782b49de44b99bb1eb77ec
Corey Kosak <coreykosak@deephaven.io>
2025-09-05 15:54:06 -0400

    maybe we can get rid of EmptyInstance

--- csharp/client/Dh_NetClient/sharabledict/SharableDict.cs
+++ csharp/client/Dh_NetClient/sharabledict/SharableDict.cs
@@ -72,29 +72,29 @@ public class SharableDict<TValue> : IReadOnlyDictionary<Int64, TValue> {
     // Manually unrolling the structure into these nested foreach is a little bit homely
     // but allows for more efficient code.
     foreach (var i0 in _root.ValiditySet) {
-      var child0 = _root.Children[i0];
-      foreach (var i1 in child0.ValiditySet) {
-        var child1 = child0.Children[i1];
-        foreach (var i2 in child1.ValiditySet) {
-          var child2 = child1.Children[i2];
-          foreach (var i3 in child2.ValiditySet) {
-            var child3 = child2.Children[i3];
-            foreach (var i4 in child3.ValiditySet) {
-              var child4 = child3.Children[i4];
-              foreach (var i5 in child4.ValiditySet) {
-                var child5 = child4.Children[i5];
-                foreach (var i6 in child5.ValiditySet) {
-                  var child6 = child5.Children[i6];
-                  foreach (var i7 in child6.ValiditySet) {
-                    var child7 = child6.Children[i7];
-                    foreach (var i8 in child7.ValiditySet) {
-                      var child8 = child7.Children[i8];
-                      foreach (var i9 in child8.ValiditySet) {
-                        var child9 = child8.Children[i9];
-                        foreach (var i10 in child9.ValiditySet) {
-                          var data = child9.Children[i10].Value;
+      var depth1 = _root.Children[i0];
+      foreach (var i1 in depth1.ValiditySet) {
+        var depth2 = depth1.Children[i1];
+        foreach (var i2 in depth2.ValiditySet) {
+          var depth3 = depth2.Children[i2];
+          foreach (var i3 in depth3.ValiditySet) {
+            var depth4 = depth3.Children[i3];
+            foreach (var i4 in depth4.ValiditySet) {
+              var depth5 = depth4.Children[i4];
+              foreach (var i5 in depth5.ValiditySet) {
+                var depth6 = depth5.Children[i5];
+                foreach (var i6 in depth6.ValiditySet) {
+                  var depth7 = depth6.Children[i6];
+                  foreach (var i7 in depth7.ValiditySet) {
+                    var depth8 = depth7.Children[i7];
+                    foreach (var i8 in depth8.ValiditySet) {
+                      var depth9 = depth8.Children[i8];
+                      foreach (var i9 in depth9.ValiditySet) {
+                        var depth10 = depth9.Children[i9];
+                        foreach (var i10 in depth10.ValiditySet) {
+                          var value = depth10.Children[i10].Value;
                           var offset = Splitter.Merge(i0, i1, i2, i3, i4, i5, i6, i7, i8, i9, i10);
-                          yield return KeyValuePair.Create(offset, data!);
+                          yield return KeyValuePair.Create(offset, value);
                         }
                       }
                     }
