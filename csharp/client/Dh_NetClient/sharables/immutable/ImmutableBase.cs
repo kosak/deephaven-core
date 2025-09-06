45929773b10c5cf3465a17365e5f291e5452d290
Corey Kosak <coreykosak@deephaven.io>
2025-09-06 16:46:55 -0400

    zmaboni time

---
+++ csharp/client/Dh_NetClient/sharables/immutable/ImmutableBase.cs
@@ -0,0 +1,15 @@
+﻿//
+// Copyright (c) 2016-2025 Deephaven Data Labs and Patent Pending
+//
+namespace Deephaven.Dh_NetClient.Sharables.Immutable;
+
+public abstract class ImmutableBase<TSelf> where TSelf : class {
+  public readonly int Count;
+
+  public ImmutableBase(int count) {
+    Count = count;
+  }
+
+  public abstract (TSelf, TSelf, TSelf) CalcDifference(TSelf target, TSelf empty);
+  public abstract void GatherNodesForUnitTesting(HashSet<object> nodes);
+}
