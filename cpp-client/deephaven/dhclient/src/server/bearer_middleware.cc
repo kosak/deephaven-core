f54558706d135c07e6e5f7ea3563abef6769e821
Corey Kosak <coreykosak@deephaven.io>
2026-04-08 20:00:45 -0400

    can

--- cpp-client/deephaven/dhclient/src/server/bearer_middleware.cc
+++ cpp-client/deephaven/dhclient/src/server/bearer_middleware.cc
@@ -14,25 +14,28 @@ namespace deephaven::client::server {
 
 // BearerMiddleware implementation
 
-BearerMiddleware::BearerMiddleware(std::string* session_token, const ExtraHeaders* extra_headers, std::mutex* mutex)
-    : session_token_(session_token), extra_headers_(extra_headers), mutex_(mutex) {}
+BearerMiddleware::BearerMiddleware(std::shared_ptr<ServerSharedState> shared_state) :
+  shared_state_(std::move(shared_state)) {}
+
+BearerMiddleware::~BearerMiddleware() = default;
 
 void BearerMiddleware::SendingHeaders(arrow::flight::AddCallHeaders* outgoing_headers) {
-  std::unique_lock<std::mutex> lock(*mutex_);
+  std::unique_lock<std::mutex> lock(shared_state_->mutex_);
   VLOG(2) << "BearerMiddleware::SendingHeaders called";
 
   // Add authorization header
-  if (!session_token_->empty()) {
+  if (!shared_state_->sessionToken_.empty()) {
     // session_token_ already contains the full authorization value (e.g., "Bearer abc123")
     // Don't add the prefix again!
-    outgoing_headers->AddHeader(std::string(kAuthorizationHeader), *session_token_);
-    VLOG(2) << "BearerMiddleware: Added authorization header, value length: " << session_token_->size();
+    outgoing_headers->AddHeader(std::string(kAuthorizationHeader), shared_state_->sessionToken_);
+    VLOG(2) << "BearerMiddleware: Added authorization header, value length: " <<
+      shared_state_->sessionToken_.size();
   } else {
     LOG(ERROR) << "BearerMiddleware: Session token is EMPTY! Cannot add authorization header";
   }
 
   // Add envoy-prefix header if present in extra headers
-  for (const auto& header : *extra_headers_) {
+  for (const auto& header : shared_state_->extraHeaders_) {
     if (header.first == kEnvoyPrefixHeader) {
       outgoing_headers->AddHeader(std::string(kEnvoyPrefixHeader), header.second);
       VLOG(2) << "BearerMiddleware: Added envoy-prefix header, value: " << header.second;
@@ -59,8 +62,8 @@ void BearerMiddleware::ReceivedHeaders(const arrow::flight::CallHeaders& incomin
       auth_value.compare(0, prefix_len, kBearerPrefix) == 0) {
     // Store the FULL authorization value (including "Bearer " prefix)
     // This matches what SendingHeaders expects
-    std::unique_lock<std::mutex> lock(*mutex_);
-    *session_token_ = std::move(auth_value);
+    std::unique_lock lock(shared_state_->mutex_);
+    shared_state_->sessionToken_ = std::move(auth_value);
     VLOG(2) << "BearerMiddleware: Updated session token from response headers";
   }
 }
@@ -71,14 +74,14 @@ void BearerMiddleware::CallCompleted(const arrow::Status& status) {
 
 // BearerMiddlewareFactory implementation
 
-BearerMiddlewareFactory::BearerMiddlewareFactory(std::string* session_token, const ExtraHeaders* extra_headers, std::mutex* mutex)
-    : session_token_(session_token), extra_headers_(extra_headers), mutex_(mutex) {}
+BearerMiddlewareFactory::BearerMiddlewareFactory(std::shared_ptr<ServerSharedState> shared_state) :
+  shared_state_(std::move(shared_state)) {}
+
+BearerMiddlewareFactory::~BearerMiddlewareFactory() = default;
 
 void BearerMiddlewareFactory::StartCall(
     const arrow::flight::CallInfo& info,
     std::unique_ptr<arrow::flight::ClientMiddleware>* middleware) {
-  *middleware = std::make_unique<BearerMiddleware>(session_token_, extra_headers_, mutex_);
+  *middleware = std::make_unique<BearerMiddleware>(shared_state_);
 }
-
 }  // namespace deephaven::client::server
-
