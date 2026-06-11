1c01abb650d056822e24139dffdc1c83db598009
Corey Kosak <coreykosak@deephaven.io>
2026-06-10 21:35:06 -0400

    I can't tell you how annoying this is

--- csharp/client/Dh_NetClientTests/RoundTripTest.cs
+++ csharp/client/Dh_NetClientTests/RoundTripTest.cs
@@ -18,17 +18,18 @@ public class RoundTripTest {
   [Fact]
   public void Nested() {
     var tm = new TableMaker();
-    tm.AddColumn<int[]>("Col1", [[0, 1, 2], [3, 4], [5]]);
+    tm.AddColumn<sbyte?[]?>("Int8", [[0, 1, 2], [3, 4, null], null, [6]]);
+    tm.AddColumn<Int16?[]?>("Int16", [[0, 1, 2], [3, 4, null], null, [6]]);
+    tm.AddColumn<Int32?[]?>("Int32", [[0, 1, 2], [3, 4, null], null, [6]]);
+    tm.AddColumn<Int64?[]?>("Int64", [[0, 1, 2], [3, 4, null], null, [6]]);
+    tm.AddColumn<float?[]?>("float", [[0.0f, 1.1f, 2.2f], [3.3f, 4.4f, null], null, [6.6f]]);
+    tm.AddColumn<double?[]?>("double", [[0.0, 1.1, 2.2], [3.3, 4.4, null], null, [6.6]]);
+    tm.AddColumn<bool?[]?>("bool", [[false, true], [false, true, null], null, [true]]);
 
     var at = tm.ToArrowTable();
 
-    // TODO(kosak): When you fix this, update DH-19910
-    var ex = Assert.Throws<Exception>(() => {
-      var ct = ArrowUtil.ToClientTable(at);
-      var at2 = ct.ToArrowTable();
-      TableComparer.AssertSame(at, at2);
-    });
-
-    Assert.Contains("Arrow type list is not supported", ex.Message);
+    var ct = ArrowUtil.ToClientTable(at);
+    var at2 = ct.ToArrowTable();
+    TableComparer.AssertSame(at, at2);
   }
 }
